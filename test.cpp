a98795f9ffa6506ea2644c772607421068cb7b7a
Marcin Buchwald <marcin.buchwald@agora.pl>
2016-10-05 14:09:04 +0200

    G++ Experiment

---
+++ test.cpp
@@ -0,0 +1,13 @@
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    vector<int> vs;
+    vs.push_back(36);
+
+    cout << "Hello GCC " << vs[0] << endl ;
+    return 0;
+}
