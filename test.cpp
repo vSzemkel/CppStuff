8ca43fb316c250ddae3031432d47c29f5e1be5b3
Marcin Buchwald <velvet@velvets-mac.local>
2016-10-28 13:04:58 +0200

    Ctrl-japko-B, fn_F5 i wypisuje do zintegrowanej konsoli

--- test.cpp
+++ test.cpp
@@ -5,8 +5,9 @@
 using namespace std;
 
 int main() {
-    vector<int> vs;
+    vector<int> vs(10);
     vs.push_back(36);
+    vs[0] = 812;
 
     cout << "Hello GCC " << vs[0] << endl ;
     return 0;
