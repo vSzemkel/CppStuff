dcd92645da35d231efe576589dd77530eecc8740
Marcin Buchwald <velvet@velvets-mac.local>
2016-11-01 21:17:48 +0100

    Wymuszenie kompilacji w standardzie C++14

--- test.cpp
+++ test.cpp
@@ -1,6 +1,7 @@
 
 #include <iostream>
 #include <vector>
+#include <tuple>
 
 using namespace std;
 
@@ -9,6 +10,9 @@ int main() {
     vs.push_back(36);
     vs[0] = 812;
 
-    cout << "Hello GCC " << vs[0] << endl ;
+    cout << "Hello GCC " << vs[0] << endl;
+
+    tuple<string, float> piwo("Okocim", 5.6);
+    cout << "Piwo " << get<string>(piwo) << " ma " << get<1>(piwo) << "% alkoholu" << endl;
     return 0;
 }
