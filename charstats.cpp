82f8d5e05830fcaf545db86c7c50645522d9f22c
Marcin Buchwald <marcin.buchwald@agora.pl>
2019-06-03 16:06:11 +0200

    [FIX] Do not output special chars

--- charstats.cpp
+++ charstats.cpp
@@ -2,8 +2,8 @@
 #include <filesystem>
 #include <iostream>
 #include <fstream>
-#include <locale>
 #include <string>
+#include <chrono>
 #include <array>
 #include <map>
 
@@ -36,6 +36,7 @@ array<diacritic_t,18> g_diacritics = {
 };
 
 int main(int argc, char* argv[]) {
+    // check input
     filesystem::path file_path;
     if (argc > 1)
         file_path = argv[1];
@@ -45,14 +46,19 @@ int main(int argc, char* argv[]) {
     }
 
     if (!filesystem::is_regular_file(file_path)) {
-        cout << file_path << " is not a regular file\n";
-        return -1;
+        cout << file_path << " is not a regular file.\n";
+        return EXIT_FAILURE;
     }
 
+    ifstream file(file_path);
+    if (file)
+        cout << "Character statistics for: " << file_path << ".\n";
+    else
+        return EXIT_FAILURE;
+
+    // compute statistics
     map<const char, uint> stats;
-    cout << "Character statistics for: " << file_path << "\n";
-    ifstream file;
-    file.open(file_path);
+    auto start = chrono::steady_clock::now();
     do {
         char c3 = '\0', c2 = '\0', c = file.get();
         for (auto& d : g_diacritics)
@@ -63,7 +69,6 @@ int main(int argc, char* argv[]) {
                     continue;
                 if (d.character.size() == 2) {
                     d.occurences++;
-                    c2 = '\0';
                     break;
                 } else {
                     if (c3 != '\0')
@@ -71,11 +76,15 @@ int main(int argc, char* argv[]) {
                     if (d.character.c_str()[2] != c3)
                         continue;
                     d.occurences++;
-                    c2 = c3 = '\0';
                     break;
                 }
             }
 
+        if (c2 != '\0') {
+            c2 = c3 = '\0';
+            continue;
+        }
+        
         const auto result = stats.emplace(c, 1);
         if (!result.second) {
             auto& stat = *result.first;
@@ -83,10 +92,19 @@ int main(int argc, char* argv[]) {
         }
     } while (file.good());
 
+    // present results
+    chrono::duration<double> duration = chrono::steady_clock::now() - start;
+    auto eof = stats.find((const char)-1);
+    if (eof != stats.end())
+        stats.erase(eof);
+    eof = stats.find((const char)10);
+    if (eof != stats.end())
+        stats.erase(eof);
     for (const auto& s : stats)
         cout << "[" << setfill('0') << setw(3) << static_cast<int>(s.first) << "] " << s.first << ": " << s.second << "\n";
     cout << "Diacritics found:\n";
     for (const auto& d : g_diacritics)
         if (d.occurences > 0)
             cout << d.character << ": " << d.occurences << "\n";
+    cout << "Processing time: " << duration.count() << " seconds.\n";
 }
