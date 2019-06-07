0bf6ee3387a13e7d95e902fd9658c05ea5e781eb
Marcin Buchwald <marcin.buchwald@agora.pl>
2019-06-07 17:14:58 +0200

    [TRY] apt-get -y install language-pack-pl

--- locale-linux.cpp
+++ locale-linux.cpp
@@ -1,28 +1,30 @@
 
 #include <string>
-#include <fcntl.h>
 #include <iostream>
 #include <algorithm>
 
 int main()
 {
-    // edit with windows1250
-    //std::wstring polish_sentence = L"za¿ó³æ gêœl¹ jaŸñ";
-    std::string polish_sentence = "za\xc5\xbc\xc3\xb3\xc5\x82\xc4\x87 g\xc4\x99\xc5\x9bl\xc4\x85 ja\xc5\xba\xc5\x84";
-
+    // edit with UTF-8
+    // check-language-support -a
+    // sudo apt-get -y install language-pack-pl
+    std::string polish_sentence = "zaÅ¼Ã³Å‚Ä‡ gÄ™Å›lÄ… jaÅºÅ„";
     std::cout << polish_sentence << "\n";
 
     std::string upper{polish_sentence};
     std::transform(std::begin(polish_sentence), std::end(polish_sentence), std::begin(upper), [](auto ch) { return toupper(ch); });
     std::cout << upper << "\n";
 
-    auto old_loc = setlocale(LC_CTYPE, "pl_PL.UTF-8");
-    std::transform(std::begin(polish_sentence), std::end(polish_sentence), std::begin(upper), [](auto ch) { return toupper(ch); });
-    std::cout << upper << "\n";
-    setlocale(LC_CTYPE, old_loc);
+    auto old_loc = setlocale(LC_CTYPE, nullptr);
+    if (setlocale(LC_CTYPE, "pl_PL.UTF-8") != nullptr) {
+        std::cout << setlocale(LC_CTYPE, nullptr) << "\n";
+        std::transform(std::begin(polish_sentence), std::end(polish_sentence), std::begin(upper), [](auto ch) { return toupper(ch); });
+        std::cout << upper << "\n";
+        setlocale(LC_CTYPE, old_loc);
+    }
 
     auto polish_locale = newlocale(LC_CTYPE, "pl_PL.UTF-8", nullptr);
-    std::transform(std::begin(polish_sentence), std::end(polish_sentence), std::begin(upper), [polish_locale](auto ch) { return toupper_l(ch, polish_locale); });
+    std::transform(std::begin(polish_sentence), std::end(polish_sentence), std::begin(upper), [&polish_locale](auto ch) { return toupper_l(ch, polish_locale); });
     std::cout << upper << "\n";
     freelocale(polish_locale);
 }
