42cb973d813c1b1047b7cd5e9ad1f03434e7da6c
Marcin Buchwald <marcin.buchwald@agora.pl>
2019-06-10 09:57:40 +0200

    [ADD] stringstream trial

--- locale-linux.cpp
+++ locale-linux.cpp
@@ -1,6 +1,7 @@
 
 #include <string>
 #include <iostream>
+#include <sstream>
 #include <algorithm>
 
 int main()
@@ -26,5 +27,25 @@ int main()
     auto polish_locale = newlocale(LC_CTYPE, "pl_PL.UTF-8", nullptr);
     std::transform(std::begin(polish_sentence), std::end(polish_sentence), std::begin(upper), [&polish_locale](auto ch) { return toupper_l(ch, polish_locale); });
     std::cout << upper << "\n";
+
+    int ch, ch2, count = 0;
+    std::stringstream ss{polish_sentence};
+    while (true) {
+        ss.read((char*)&ch, 1);
+        if (!ss.good())
+            break;
+        if ((ch & 0xf0) == 0xc0) {
+            ss.read((char*)&ch2, 1);
+            ch += ch2 * 0x100;
+            printf("-%x-", ch);
+            ch = std::use_facet<std::ctype<wchar_t>>(std::locale("pl_PL.UTF-8")).toupper(ch);
+            printf(">%x-", ch);
+            std::cout << (char)ch;
+        } else
+            std::cout << std::use_facet<std::ctype<char>>(std::locale("pl_PL.UTF-8")).toupper(ch);
+        ++count;
+    }
+
+    std::cout << "\nstd::size(): " << polish_sentence.size() << ", characters count: " << count << "\n";
     freelocale(polish_locale);
 }
