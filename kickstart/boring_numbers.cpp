1e68c9fd9b83e3a4b75899a097b87de84c77dec4
Marcin Buchwald <marcin.buchwald@outlook.com>
2020-11-16 07:52:07 +0100

    [FIX] Boring numbers

--- kickstart/boring_numbers.cpp
+++ kickstart/boring_numbers.cpp
@@ -22,13 +22,12 @@ bool is_boring(const int64_t n)
 int64_t boring_ceil(int64_t n)
 {
     const int digits = 1 + (int)log10(n);
-    for (int pos = digits; pos > 0; --pos) {
+    for (int pos = 1; pos <= digits; ++pos) {
         const int64_t p10 = pow(10, pos - 1);
         const int d = (n / p10) % 10;
         if (((digits - pos) % 2) == (d % 2)) {
             n = ((n + p10) / p10) * p10;
             n += std::atoll(g_min_boring.substr(digits - pos + 1, pos - 1).c_str());
-            break;
         }
     }
 
@@ -41,13 +40,12 @@ int64_t boring_ceil(int64_t n)
 int64_t boring_floor(int64_t n)
 {
     const int digits = 1 + (int)log10(n);
-    for (int pos = digits; pos > 0; --pos) {
+    for (int pos = 1; pos <= digits; ++pos) {
         const int64_t p10 = pow(10, pos - 1);
         const int d = (n / p10) % 10;
         if (((digits - pos) % 2) == (d % 2)) {
             n = ((n - p10) / p10) * p10;
             n += std::atoll(g_max_boring.substr(digits - pos + 1, pos - 1).c_str());
-            break;
         }
     }
 
@@ -73,17 +71,17 @@ int64_t smart()
     int64_t ret{0};
     int64_t a = boring_ceil(g_a);
     int64_t b = boring_floor(g_b);
-    const int digits_a = 1 + log10(a);
-    const int digits_b = 1 + log10(b);
+    const int64_t b_copy{b};
+    const int digits_a = 1 + (int)log10(a);
+    const int digits_b = 1 + (int)log10(b);
     for (int i = digits_a; i < digits_b; ++i) {
-        b = std::atoll(g_max_boring.substr(0, i).c_str());
-        if (b >= a)
-            ret += boring_ordinal(b - a) + 1;
+        b = std::atoll(g_max_boring.substr(0, i).c_str()) - a;
+        if (b >= 0)
+            ret += boring_ordinal(b) + 1;
         a = std::atoll(g_min_boring.substr(0, i + 1).c_str());
     }
 
-    b = boring_floor(g_b);
-    b -= a;
+    b = b_copy - a;
     if (b >= 0)
         ret += boring_ordinal(b) + 1;
 
@@ -99,42 +97,6 @@ int64_t brute() {
     return ret;
 }
 
-int64_t dull_subtask(int64_t n)
-{
-    const int last = n % 10;
-    int64_t ret = (g_a <= n && n <= g_b) ? 1 : 0;
-
-    n *= 10;
-    if (n <= g_b) {
-        if (last % 2 == 0) {
-            ret += dull_subtask(n + 1);
-            ret += dull_subtask(n + 3);
-            ret += dull_subtask(n + 5);
-            ret += dull_subtask(n + 7);
-            ret += dull_subtask(n + 9);
-        } else {
-            ret += dull_subtask(n + 2);
-            ret += dull_subtask(n + 4);
-            ret += dull_subtask(n + 6);
-            ret += dull_subtask(n + 8);
-            ret += dull_subtask(n + 0);
-        }
-    }
-    
-    return ret;
-}
-
-int64_t dull() {
-    int64_t ret{0};
-    ret += dull_subtask(1);
-    ret += dull_subtask(3);
-    ret += dull_subtask(5);
-    ret += dull_subtask(7);
-    ret += dull_subtask(9);
-
-    return ret;
-}
-
 int main(int argc, char* argv[])
 {
     std::ios_base::sync_with_stdio(false);
@@ -146,7 +108,7 @@ int main(int argc, char* argv[])
     for (int g = 1; g <= no_of_cases; ++g) {
         std::cin >> g_a >> g_b;
         std::cout << "Case #" << g << ": " << smart() << "\n";
-        std::cout << "Case #" << g << ": " << brute() << "\n";
+        // std::cout << "Case #" << g << ": " << brute() << "\n";
     }
 }
 
