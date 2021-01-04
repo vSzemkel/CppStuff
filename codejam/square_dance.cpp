8d19161f05537ff200500e61c7eb723f4def798a
Marcin Buchwald <marcin.buchwald@outlook.com>
2021-01-04 12:47:16 +0100

    [ADD] Square dance, single dimention board

--- codejam/square_dance.cpp
+++ codejam/square_dance.cpp
@@ -1,113 +1,83 @@
 
 #include <algorithm>
 #include <iostream>
-#include <utility>
 #include <vector>
 
 // Square Dance
 // https://codingcompetitions.withgoogle.com/codejam/round/000000000019fd74/00000000002b1355
 
-using coord_t = std::pair<int, int>;
-
 struct dancer_t
 {
     int skill;
-    int N, S, E, W;
-    int dist_N, dist_S, dist_E, dist_W;
+    int tr; // top active row
+    int br; // bottom
+    int lc; // left active column
+    int rc; // right
     bool well_dancing;
 };
 
 int g_rows, g_cols;
-std::vector<std::vector<dancer_t>> g_board;
+std::vector<dancer_t> g_board;
 
 void quallify(dancer_t& d)
 {
-    int sum{0}, cnt{0};
-    if (d.N > 0) { sum += d.N; ++cnt; }
-    if (d.S > 0) { sum += d.S; ++cnt; }
-    if (d.E > 0) { sum += d.E; ++cnt; }
-    if (d.W > 0) { sum += d.W; ++cnt; }
+    int sum{0};
+    if (d.tr >= 0) sum += d.skill - g_board[d.tr].skill;
+    if (d.br >= 0) sum += d.skill - g_board[d.br].skill;
+    if (d.rc >= 0) sum += d.skill - g_board[d.rc].skill;
+    if (d.lc >= 0) sum += d.skill - g_board[d.lc].skill;
 
-    d.well_dancing = cnt == 0 || d.skill >= (double)sum / cnt;
+    d.well_dancing = sum >= 0;
 }
 
 int64_t solve() {
     std::cin >> g_rows >> g_cols;
-    g_board.assign(g_rows, std::vector<dancer_t>(g_cols));
+    const int g_size = g_rows * g_cols;
+    g_board.resize(g_size);
     // read input
-    for (int r = 0; r < g_rows; ++r)
-        for (int c = 0; c < g_cols; ++c)
-            std::cin >> g_board[r][c].skill;
+    for (int d = 0; d < g_size; ++d)
+        std::cin >> g_board[d].skill;
     // initialize
-    std::vector<coord_t> still_dancing;
     int64_t ret{0}, survivors_skills{0};
-    for (int r = 0; r < g_rows; ++r)
-        for (int c = 0; c < g_cols; ++c) {
-            auto& d = g_board[r][c];
-            if (c > 0) {
-                d.W = g_board[r][c - 1].skill;
-                d.dist_W = 1;
-            }
-            if (c < g_cols - 1) {
-                d.E = g_board[r][c + 1].skill;
-                d.dist_E = 1;
-            }
-            if (r > 0) {
-                d.N = g_board[r - 1][c].skill;
-                d.dist_N = 1;
-            }
-            if (r < g_rows - 1) {
-                d.S = g_board[r + 1][c].skill;
-                d.dist_S = 1;
-            }
-            quallify(d);
-            survivors_skills += d.skill;
-            still_dancing.emplace_back(r, c);
-        }
+    std::vector<int> still_dancing(g_size);
+    for (int i = 0; i < g_size; ++i) {
+        auto& d = g_board[i];
+        d.lc = i % g_cols > 0 ? i - 1 : -1;
+        d.rc = i % g_cols < g_cols - 1 ? i + 1 : -1;
+        d.tr = i / g_cols > 0 ? i - g_cols : -1;
+        d.br = i / g_cols < g_rows - 1 ? i + g_cols : -1;
+        quallify(d);
+        still_dancing[i] = i;
+        survivors_skills += d.skill;
+    }
     // play rounds
     while (true) {
         ret += survivors_skills;
-        const auto victims = std::partition(still_dancing.begin(), still_dancing.end(), [](const coord_t& d){ return g_board[d.first][d.second].well_dancing; });
+        const auto victims = std::partition(still_dancing.begin(), still_dancing.end(), [](const int pos){ return g_board[pos].well_dancing; });
         if (victims == still_dancing.end()) break;
 
         for (auto v = victims; v != still_dancing.end(); ++v) {
-            const auto [r, c] = *v;
-            const auto& d = g_board[r][c];
+            const int pos = *v;
+            const auto& d = g_board[pos];
             survivors_skills -= d.skill;
-            if (c > 0 && d.dist_W > 0) {
-                auto& w = g_board[r][c - d.dist_W];
-                if (d.dist_E > 0) {
-                    w.E = d.E;
-                    w.dist_E += d.dist_E;
-                } else
-                    w.dist_E = w.E = 0;
+            if (d.lc >= 0) {
+                auto& w = g_board[d.lc];
+                w.rc = d.rc;
                 quallify(w);
             }
-            if (c < g_cols - 1 && d.dist_E > 0) {
-                auto& e = g_board[r][c + d.dist_E];
-                if (d.dist_W > 0) {
-                    e.W = d.W;
-                    e.dist_W += d.dist_W;
-                } else
-                    e.dist_W = e.W = 0;
+            if (d.rc >= 0) {
+                auto& e = g_board[d.rc];
+                e.lc = d.lc;
                 quallify(e);
             }
-            if (r > 0 && d.dist_N > 0) {
-                auto& n = g_board[r - d.dist_N][c];
-                if (d.dist_S > 0) {
-                    n.S = d.S;
-                    n.dist_S += d.dist_S;
-                } else
-                    n.dist_S = n.S = 0;
+            if (d.tr >= 0) {
+                auto& n = g_board[d.tr];
+                n.br = d.br;
                 quallify(n);
             }
-            if (r < g_rows - 1 && d.dist_S > 0) {
-                auto& s = g_board[r + d.dist_S][c];
-                if (d.dist_S > 0) {
-                    s.N = d.N;
-                    s.dist_N += d.dist_N;
-                } else
-                    s.dist_N = s.N = 0;
+            if (d.br >= 0) {
+                auto& s = g_board[d.br];
+                s.tr = d.tr;
                 quallify(s);
             }
         }
@@ -124,11 +94,9 @@ int main(int, char**)
     std::cin.tie(nullptr);
     std::cout.tie(nullptr);
 
-    int no_of_cases;
-    std::cin >> no_of_cases;
-    for (int g = 1; g <= no_of_cases; ++g) {
+    int no_of_cases; std::cin >> no_of_cases;
+    for (int g = 1; g <= no_of_cases; ++g)
         std::cout << "Case #" << g << ": " << solve() << "\n";
-    }
 }
 
 /*
---
+++ codejam/square_dance2.cpp
@@ -0,0 +1,171 @@
+
+#include <algorithm>
+#include <iostream>
+#include <utility>
+#include <vector>
+
+// Square Dance
+// https://codingcompetitions.withgoogle.com/codejam/round/000000000019fd74/00000000002b1355
+
+using coord_t = std::pair<int, int>;
+
+struct dancer2_t
+{
+    int skill;
+    int N, S, E, W;
+    int dist_N, dist_S, dist_E, dist_W;
+    bool well_dancing;
+};
+
+int g_rows, g_cols;
+std::vector<std::vector<dancer2_t>> g_board2;
+
+void quallify2(dancer2_t& d)
+{
+    int sum{0};
+    if (d.N > 0) sum += d.skill - d.N;
+    if (d.S > 0) sum += d.skill - d.S;
+    if (d.E > 0) sum += d.skill - d.E;
+    if (d.W > 0) sum += d.skill - d.W;
+
+    d.well_dancing = sum >= 0;
+}
+
+void quallify3(dancer2_t& d)
+{
+    int sum{0}, cnt{0};
+    if (d.N > 0) { sum += d.N; ++cnt; }
+    if (d.S > 0) { sum += d.S; ++cnt; }
+    if (d.E > 0) { sum += d.E; ++cnt; }
+    if (d.W > 0) { sum += d.W; ++cnt; }
+
+    d.well_dancing = cnt == 0 || d.skill >= (double)sum / cnt;
+}
+
+int64_t solve2() {
+    std::cin >> g_rows >> g_cols;
+    g_board2.assign(g_rows, std::vector<dancer2_t>(g_cols));
+    // read input
+    for (int r = 0; r < g_rows; ++r)
+        for (int c = 0; c < g_cols; ++c)
+            std::cin >> g_board2[r][c].skill;
+    // initialize
+    std::vector<coord_t> still_dancing;
+    int64_t ret{0}, survivors_skills{0};
+    for (int r = 0; r < g_rows; ++r)
+        for (int c = 0; c < g_cols; ++c) {
+            auto& d = g_board2[r][c];
+            if (c > 0) {
+                d.W = g_board2[r][c - 1].skill;
+                d.dist_W = 1;
+            }
+            if (c < g_cols - 1) {
+                d.E = g_board2[r][c + 1].skill;
+                d.dist_E = 1;
+            }
+            if (r > 0) {
+                d.N = g_board2[r - 1][c].skill;
+                d.dist_N = 1;
+            }
+            if (r < g_rows - 1) {
+                d.S = g_board2[r + 1][c].skill;
+                d.dist_S = 1;
+            }
+            quallify2(d);
+            survivors_skills += d.skill;
+            still_dancing.emplace_back(r, c);
+        }
+    // play rounds
+    while (true) {
+        ret += survivors_skills;
+        const auto victims = std::partition(still_dancing.begin(), still_dancing.end(), [](const coord_t& d){ return g_board2[d.first][d.second].well_dancing; });
+        if (victims == still_dancing.end()) break;
+
+        for (auto v = victims; v != still_dancing.end(); ++v) {
+            const auto [r, c] = *v;
+            const auto& d = g_board2[r][c];
+            survivors_skills -= d.skill;
+            if (c > 0 && d.dist_W > 0) {
+                auto& w = g_board2[r][c - d.dist_W];
+                if (d.dist_E > 0) {
+                    w.E = d.E;
+                    w.dist_E += d.dist_E;
+                } else
+                    w.dist_E = w.E = 0;
+                quallify2(w);
+            }
+            if (c < g_cols - 1 && d.dist_E > 0) {
+                auto& e = g_board2[r][c + d.dist_E];
+                if (d.dist_W > 0) {
+                    e.W = d.W;
+                    e.dist_W += d.dist_W;
+                } else
+                    e.dist_W = e.W = 0;
+                quallify2(e);
+            }
+            if (r > 0 && d.dist_N > 0) {
+                auto& n = g_board2[r - d.dist_N][c];
+                if (d.dist_S > 0) {
+                    n.S = d.S;
+                    n.dist_S += d.dist_S;
+                } else
+                    n.dist_S = n.S = 0;
+                quallify2(n);
+            }
+            if (r < g_rows - 1 && d.dist_S > 0) {
+                auto& s = g_board2[r + d.dist_S][c];
+                if (d.dist_S > 0) {
+                    s.N = d.N;
+                    s.dist_N += d.dist_N;
+                } else
+                    s.dist_N = s.N = 0;
+                quallify2(s);
+            }
+        }
+
+        still_dancing.erase(victims, still_dancing.end());
+    }
+
+    return ret;
+}
+
+int main(int, char**)
+{
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+
+    int no_of_cases;
+    std::cin >> no_of_cases;
+    for (int g = 1; g <= no_of_cases; ++g) {
+        std::cout << "Case #" << g << ": " << solve() << "\n";
+    }
+}
+
+/*
+clang++.exe -Wall -Wextra -ggdb3 -O0 -std=c++17 square_dance.cpp -o square_dance.exe
+g++ -Wall -Wextra -ggdb3 -Og -std=c++17 square_dance.cpp -o square_dance.o
+square_dance.exe < square_dance.in
+
+Input:
+
+4
+1 1
+15
+3 3
+1 1 1
+1 2 1
+1 1 1
+1 3
+3 1 2
+1 3
+1 2 3
+
+Output:
+
+Case #1: 15
+Case #2: 16
+Case #3: 14
+Case #4: 14
+
+*/
