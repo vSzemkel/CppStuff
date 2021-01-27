88ddab36125dffd74c9cd7647ed0bd545e26fcde
Marcin Buchwald <marcin.buchwald@outlook.com>
2021-01-27 16:33:56 +0100

    [ADD] Simple problem in USACO format

---
+++ usaco/ride.cpp
@@ -0,0 +1,47 @@
+/*
+ID: marcin.8
+LANG: C++
+TASK: ride
+*/
+
+#include <iostream>
+#include <fstream>
+
+std::ifstream task_in("ride.in");
+std::ofstream task_out("ride.out");
+
+uint64_t decrypt(const std::string& s)
+{
+    uint64_t ret{1};
+    for (const char c : s)
+        ret *= (c - 'A' + 1);
+    return ret % 47;
+}
+
+int main(int, char**)
+{
+    std::string comet, group;
+    task_in >> comet >> group;
+    task_out << (decrypt(comet) == decrypt(group) ? "GO" : "STAY") << "\n";
+}
+
+/*
+
+Compile:
+clang++.exe -Wall -Wextra -ggdb3 -O0 -std=c++17 ride.cpp -o ride.exe
+g++ -Wall -Wextra -ggdb3 -Og -std=c++17 ride.cpp -o ride.o
+
+Run:
+py.exe interactive_runner.py py.exe ride_testing_tool.py 1 -- ride.exe
+ride.exe < ride.in
+
+Input:
+
+COMETQ
+HVNGAT
+
+Output:
+
+GO
+
+*/
