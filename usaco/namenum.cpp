dd534788368551801746ca96eb6a22943f73e6b9
Marcin Buchwald <marcin.buchwald@outlook.com>
2021-02-11 18:34:45 +0100

    [ADD] Namenum smart

--- usaco/namenum.cpp
+++ usaco/namenum.cpp
@@ -17,6 +17,32 @@ std::ofstream task_out("namenum.out");
 
 int main(int, char**)
 {
+    std::unordered_map<char, char> mapping = {
+        {'A', '2'}, {'B', '2'}, {'C', '2'},
+        {'D', '3'}, {'E', '3'}, {'F', '3'},
+        {'G', '4'}, {'H', '4'}, {'I', '4'},
+        {'J', '5'}, {'K', '5'}, {'L', '5'},
+        {'M', '6'}, {'N', '6'}, {'O', '6'},
+        {'P', '7'}, {'R', '7'}, {'S', '7'},
+        {'T', '8'}, {'U', '8'}, {'V', '8'},
+        {'W', '9'}, {'X', '9'}, {'Y', '9'}
+    };
+
+    std::string number; task_in >> number;
+    std::ifstream dict_in("dict.txt");
+    while (dict_in.good()) {
+        std::string tr, n; dict_in >> n;
+        std::transform(n.begin(), n.end(), std::back_inserter(tr), [&mapping](const char c){ return mapping[c]; });
+        if (tr == number)
+            task_out << n << "\n";
+    }
+
+    if (task_out.tellp() == 0)
+        task_out << "NONE\n";
+}
+
+void not_so_smart()
+{
     std::unordered_map<char, std::vector<char>> mapping = {
         {'2', {'A', 'B', 'C'}},
         {'3', {'D', 'E', 'F'}},
