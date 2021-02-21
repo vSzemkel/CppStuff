7ecec5f29f06bd4040191d8293a38749152df7a2
Marcin Buchwald <marcin.buchwald@outlook.com>
2021-02-21 11:04:39 +0100

    [ADD] String polynomial hasher

--- classic/polynomial_hasher.cpp
+++ classic/polynomial_hasher.cpp
@@ -1,12 +1,17 @@
 
 #include <assert.h>
 #include <iostream>
+#include <numeric>
 #include <optional>
+#include <string>
+#include <utility>
+#include <vector>
 
-// Modnum - integral datatype implementing modulo arithmetic
+// Polynomial rolling hasher
+// Using modnum struct of ecnerwala
 
-template <int M = 998244353>
-class modnum_t {
+template <int M>
+struct modnum_t {
   public:
     modnum_t() : value(0) {}
     modnum_t(int64_t v) : value(v % M) {
@@ -116,23 +121,83 @@ class modnum_t {
     int value;
 };
 
+class string_hasher
+{
+    using modn_t = modnum_t<998244353>;
+  public:
+    string_hasher(const std::string& s) : _size((int)s.size()) {
+        assert(!s.empty());
+        init_powers(_size);
+        _prefix_hash.resize(_size);
+        for (int i = 0; i < _size; ++i)
+            _prefix_hash[i] = POWERS[i] * (s[i] - 'A' + 1);
+        std::inclusive_scan(_prefix_hash.begin(), _prefix_hash.end(), _prefix_hash.begin());
+    }
+
+    int hash(const int begin, const int end) const { // [begin, end)
+        assert(0 <= begin && begin < end && end <= _size);
+        if (begin == 0) return (int)_prefix_hash[end - 1];
+        return (int)((_prefix_hash[end - 1] - _prefix_hash[begin - 1]) * POWERS[begin].inv());
+    }
+
+    int hash() const {
+        return (int)_prefix_hash.back();
+    }
+
+    friend bool operator==(const string_hasher& a, const string_hasher& b) {
+        return a._size == b._size && a._prefix_hash.back() == b._prefix_hash.back();
+    }
+
+    friend bool operator<(const string_hasher& a, const string_hasher& b) {
+        return std::make_pair(a._size, a._prefix_hash.back()) < std::make_pair(b._size, b._prefix_hash.back());
+    }
+
+  private:
+    void init_powers(const int len) {
+        int psize = (int)POWERS.size();
+        if (psize < len + 1) {
+            POWERS.reserve(len + 1);
+            if (POWERS.empty()) {
+                POWERS.push_back(1);
+                ++psize;
+            }
+            while (psize < len + 1) {
+                POWERS.push_back(POWERS.back() * BASE);
+                ++psize;
+            }
+        }
+    }
+
+    int _size;
+    std::vector<modn_t> _prefix_hash;
+    static constexpr int BASE = 31; // 53 for mixed case
+    static inline std::vector<modn_t> POWERS;
+};
+
 int main(int, char**)
 {
-    modnum_t<20> m = 13;
-    assert((int)inv(m).value() == 17); // 13 * 17 == 1 (mod 20)
-    modnum_t<20> m2 = 15;
-    assert(!inv(m2).has_value());
-
-    assert((int)neg(m2) == 5);
-    auto m3 = m2++;
-    assert((int)m2 == 16 && (int)m3 == 15);
-    assert((int)(m2 * m3) == 0);
+    string_hasher sh1{"ALA MA KOTA BUREGO"};
+    string_hasher sh2{"KOTA"};
+    assert(sh1.hash(7, 11) == sh2.hash());
+    string_hasher sh3{"clang++.exe -Wall -Wextra -ggdb3 -O0 -std=c++17 polynomial_hasher.cpp -o polynomial_hasher.exe"};
+    string_hasher sh4{"polynomial"};
+    assert(sh3.hash(48, 58) == sh4.hash());
+    string_hasher sh5{"ecnerwala"};
+    string_hasher sh6{"ecnerwala"};
+    assert(sh5 == sh6);
+    string_hasher sh7{"ABC"};
+    string_hasher sh8{"ACC"};
+    assert(sh7 < sh8);
+    std::cout << "PASSED\n";
 }
 
 /*
 
 Compile:
-clang++.exe -Wall -Wextra -ggdb3 -O0 -std=c++17 modnum.cpp -o modnum.exe
-g++ -Wall -Wextra -ggdb3 -Og -std=c++17 modnum.cpp -o modnum.o
+clang++.exe -Wall -Wextra -ggdb3 -O0 -std=c++17 polynomial_hasher.cpp -o polynomial_hasher.exe
+g++ -Wall -Wextra -ggdb3 -Og -std=c++17 polynomial_hasher.cpp -o polynomial_hasher.o
+
+Run:
+polynomial_hasher.exe
 
 */
