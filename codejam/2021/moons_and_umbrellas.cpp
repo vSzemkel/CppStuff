14be9c739aa64ade606be34305905861714748f4
Marcin Buchwald <marcin.buchwald@outlook.com>
2021-03-28 10:54:05 +0200

    [FIX] Moons and umrellas revisited

--- codejam/2021/moons_and_umbrellas.cpp
+++ codejam/2021/moons_and_umbrellas.cpp
@@ -2,75 +2,41 @@
 #include <algorithm>
 #include <iostream>
 #include <string>
-#include <vector>
 
-static void ecnerwala()
-{
-    int X, Y; std::string text; std::cin >> X >> Y >> text;
-
-    const int INF = 1e9;
-    std::vector<int> dp(2);
-    for (int i = 0; i < int(text.size()); i++) {
-        if (i) {
-            dp = {std::min(dp[0], dp[1] + Y), std::min(dp[1], dp[0] + X)};
-        }
-        char c = text[i];
-        if (c == 'C')
-            dp[1] = INF;
-        else if (c == 'J')
-            dp[0] = INF;
-    }
-
-    std::cout << std::min(dp[0], dp[1]);
-}
-
-static void smart()
-{
-    int X, Y; std::string text; std::cin >> X >> Y >> text;
-
-    std::string cleaned;
-    std::copy_if(text.begin(), text.end(), std::back_inserter(cleaned), [](const char c){
-        return c != '?';
-    });
-
-    int cost{0};
-    for (int i = 0; i < (int)cleaned.size() - 1; ++i) {
-        if (cleaned[i] == 'C' && cleaned[i + 1] == 'J') cost += X;
-        if (cleaned[i] == 'J' && cleaned[i + 1] == 'C') cost += Y;
-    }
-
-    std::cout << cost;
-}
 
 // CJ cost X
 // JC cost Y
+static void smart() // inspired by ecnerwala
+{
+    int X, Y; std::string text; std::cin >> X >> Y >> text;
+    const int INF = 1e9, size = (int)text.size();
+
+    int pos{0}, costIfNextC{0}, costIfNextJ{0};
+    while (pos < size) {
+        if (pos > 0) {
+            const int oldCostIfNextC = costIfNextC;
+            costIfNextC = std::min(costIfNextJ + Y, costIfNextC);
+            costIfNextJ = std::min(oldCostIfNextC + X, costIfNextJ);
+        }
+        const char cur = text[pos++];
+        if (cur == 'J') costIfNextC = INF;
+        else if (cur == 'C') costIfNextJ = INF;
+    }
+
+    std::cout << std::min(costIfNextC, costIfNextJ);
+}
+
 static void solve()
 {
     int X, Y; std::string text; std::cin >> X >> Y >> text;
-    int size = (int)text.size();
+    const int size = (int)text.size();
 
-    int lpos{0};
-    while (lpos < size && text[lpos] == '?') ++lpos;
-    int rpos{size - 1};
-    while (rpos >=0 && text[rpos] == '?') --rpos;
-    if (rpos <= lpos) {
-        std::cout << 0;
-        return;
-    }
-
-    size = rpos - lpos + 1;
-    text = text.substr(lpos, size);
-
+    char prev = '-';
     int pos{0}, cost{0};
-    char prev = text[0];
     while (pos < size) {
-        while (pos < size && text[pos] == prev) ++pos;
+        while (pos < size && (text[pos] == prev || text[pos] == '?')) ++pos;
         if (pos == size) break;
-        char cur = text[pos];
-        if (cur == '?') {
-            while (text[pos] == '?') ++pos;
-            cur = text[pos];
-        }
+        const char cur = text[pos];
         if (prev == 'C' && cur == 'J') cost += X;
         if (prev == 'J' && cur == 'C') cost += Y;
         prev = cur;
@@ -88,7 +54,7 @@ int main(int, char**)
     int no_of_cases;
     std::cin >> no_of_cases;
     for (int g = 1; g <= no_of_cases; ++g) {
-        std::cout << "Case #" << g << ": "; ecnerwala(); std::cout << '\n';
+        std::cout << "Case #" << g << ": "; smart(); std::cout << '\n';
     }
 }
 
