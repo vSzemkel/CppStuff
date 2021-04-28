1abeef30cab218b01e88f5794eeabd1b73a5b1cd
Marcin Buchwald <marcin.buchwald@outlook.com>
2021-04-28 11:15:21 +0200

    [WRK] Broken clock

--- codejam/2021/broken_clock.cpp
+++ codejam/2021/broken_clock.cpp
@@ -2,64 +2,69 @@
 #include <algorithm>
 #include <array>
 #include <assert.h>
-#include <bitset>
-#include <cmath>
-#include <iomanip>
 #include <iostream>
-#include <iterator>
-#include <filesystem>
-#include <fstream>
-#include <functional>
-#include <limits>
-#include <map>
-#include <memory>
 #include <numeric>
-#include <optional>
-#include <queue>
-#include <random>
-#include <set>
-#include <stdlib.h>
-#include <string>
-#include <string_view>
-#include <unordered_map>
-#include <unordered_set>
-#include <tuple>
-#include <utility>
-#include <vector>
 
 // Broken clock
 // https://codingcompetitions.withgoogle.com/codejam/round/0000000000435baf/00000000007ae694
 
-constexpr int minutesPerHour = 60;
-constexpr int secondsPerMinute = 60;
+constexpr int64_t minutesPerHour = 60;
+constexpr int64_t secondsPerMinute = 60;
 constexpr int64_t nanosecondsPerSecond = 1'000'000'000;
 constexpr int64_t nanosecondsPerMinute = nanosecondsPerSecond * 60;
 constexpr int64_t nanosecondsPerHour = nanosecondsPerMinute * 60;
+// angleXxxxPerNanosecond is a measure of speed (distance / time)
 constexpr int64_t angleHourPerNanosecond = 1;
 constexpr int64_t angleMinutePerNanosecond = angleHourPerNanosecond * 60 / 5;
 constexpr int64_t angleSecondPerNanosecond = angleMinutePerNanosecond * 60 / 1;
 constexpr int64_t angleFullHour = angleHourPerNanosecond * nanosecondsPerHour;
 constexpr int64_t angleFullMinute = angleMinutePerNanosecond * nanosecondsPerMinute;
 constexpr int64_t angleFullSecond = angleSecondPerNanosecond * nanosecondsPerSecond;
+constexpr int64_t angle360 = angleFullSecond * secondsPerMinute;
+
+static bool check(const int64_t h, const int64_t m, const int64_t s)
+{
+    const auto HH = h / angleFullHour;
+    const auto MM = m / angleFullMinute;
+    const auto SS = s / angleFullSecond;
+    const auto FF = s % angleFullSecond;
+
+    const auto checkedM = MM == minutesPerHour * (h % angleFullHour) / angleFullHour;
+    const auto checkedS = SS == secondsPerMinute * (m % angleFullMinute) / angleFullMinute;
+    const auto checkedN = 0 == nanosecondsPerSecond * (s % angleFullSecond) / angleFullSecond;
+
+    if (HH < 12 && MM < 60 && SS < 60 && checkedM && checkedS && checkedN) {
+        const auto NN = 0;//nanosecondsPerSecond * (T[2] % angleFullSecond) * angleFullSecond;
+        std::cout << HH << ' ' << MM << ' ' << SS << ' ' << NN;
+        return true;
+    }
+
+    return false;
+}
 
 static void solve() {
     std::array<int64_t, 3> T;
     std::cin >> T[0] >> T[1] >> T[2];
 
-    do { // assume T is HH:MM:SS
-        const int HH = T[0] / angleFullHour;
-        const int MM = T[1] / angleFullMinute;
-        const int SS = T[2] / angleFullSecond;
+    std::sort(T.begin(), T.end());
+    T[2] -= T[0];
+    T[1] -= T[0];
+    T[0] = 0;
 
-        const auto maybeMM = minutesPerHour * (T[0] % angleFullHour) / angleFullHour;
-        const auto maybeSS = secondsPerMinute * (T[1] % angleFullMinute) / angleFullMinute;
+    for (int r = 0; r < secondsPerMinute; ++r) {
+        do { // assume T is HH:MM:SS
+            const auto secondsAdjustment = (T[2] / angleFullSecond * angleFullSecond) - T[2];
+            const auto T0 = T[0] + secondsAdjustment;
+            const auto T1 = T[1] + secondsAdjustment;
+            const auto T2 = T[2] + secondsAdjustment;
 
-        if (HH < 12 && MM < 60 && SS < 60 && maybeMM == MM && maybeSS == SS) {
-            const int NN = nanosecondsPerSecond * (T[2] % angleFullSecond) * angleFullSecond;
-            std::cout << HH << ' ' << MM << ' ' << SS << ' ' << NN;
-            return;
-        }
-    } while (std::next_permutation(T.begin(), T.end()));
+            if (check(T0, T1, T2))
+                return;
+        } while (std::next_permutation(T.begin(), T.end()));
+
+        for (auto& a : T)
+            a = (a + angleFullSecond) % angle360;
+    }
 }
 
 int main(int, char**)
@@ -72,7 +77,6 @@ int main(int, char**)
     std::cin >> no_of_cases;
     for (int g = 1; g <= no_of_cases; ++g) {
         std::cout << "Case #" << g << ": "; solve(); std::cout << '\n';
-        //std::cout << "Case #" << g << ": " << std::fixed << std::setprecision(15) << solve() << '\n';
     }
 }
 
@@ -87,15 +91,23 @@ broken_clock.exe < broken_clock.in
 
 Input:
 
-3
+7
 0 0 0
 0 21600000000000 23400000000000
 1476000000000 2160000000000 3723000000000
+5400000000000 5400000000000 5400000000000
+10800000000000 32400000000000 34200000000000
+23076000000000 23760000000000 25323000000000
+0 11 719
 
 Output:
 
 Case #1: 0 0 0 0
 Case #2: 6 30 0 0
 Case #3: 1 2 3 0
+Case #4: 0 0 0 0
+Case #5: 0 30 0 0
+Case #6: 1 2 3 0
+Case #7: 0 0 0 1
 
 */
