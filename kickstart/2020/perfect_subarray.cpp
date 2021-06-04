3d6f334be7c2935dc356b565c930b54b0affe80e
Marcin Buchwald <marcin.buchwald@outlook.com>
2021-06-04 10:07:46 +0200

    [ADD] Perfect subarray optimal solution

--- kickstart/2020/perfect_subarray.cpp
+++ kickstart/2020/perfect_subarray.cpp
@@ -1,60 +1,41 @@
 
-#include <algorithm>
-#include <array>
-#include <assert.h>
-#include <bitset>
 #include <cmath>
-#include <iomanip>
 #include <iostream>
-#include <iterator>
-#include <filesystem>
-#include <fstream>
-#include <functional>
-#include <limits>
-#include <map>
-#include <memory>
 #include <numeric>
-#include <optional>
-#include <queue>
-#include <random>
-#include <set>
-#include <stdlib.h>
-#include <string>
-#include <string_view>
-#include <unordered_map>
-#include <unordered_set>
-#include <tuple>
-#include <utility>
 #include <vector>
 
 // Perfect Subarray
 // https://codingcompetitions.withgoogle.com/kickstart/round/000000000019ff43/00000000003381cb
 
-std::vector<int64_t> g_input, g_partial;
-std::unordered_set<int64_t> g_squares;
+std::vector<int> g_input, g_partial, g_squares;
 
 constexpr int MAXSUM = 10'000'000;
 
 const auto range_sum = [](const int i, const int j){return g_partial[j + 1] - g_partial[i];};
 
-static void solve() {
+static void solve() { // adapted from Errichto
     int N; std::cin >> N;
     g_input.resize(N);
-    for (auto& n : g_input)
+    int abssum{0}, prefsum{0};
+    for (auto& n : g_input) {
         std::cin >> n;
+        abssum += std::abs(n);
+    }
 
-    g_partial.resize(N + 1);
-    std::partial_sum(g_input.begin(), g_input.end(), g_partial.begin() + 1);
+    // cnt[abssum + X] is the number of prefixes g_input[0..i] giving the sum X
+    std::vector<int> cnt(2 * abssum + 1);
+    cnt[abssum] = 1; // for the empty subarray
 
     int64_t ret{0};
-    for (int len = 1; len <= N; ++len)
-        for (int pos = 0; pos + len <= N; ++pos) {
-            const auto ran = range_sum(pos, pos + len - 1);
-            //const auto can = int64_t(std::sqrt(ran));
-            //if (can * can == ran)
-            if (g_squares.count(ran) > 0)
-                ++ret;
+    for (int i = 0; i < N; ++i) {
+        prefsum += g_input[i];
+        const auto addr = abssum + prefsum;
+        for (const int s : g_squares) {
+            if (addr < s) break;
+            ret += cnt[addr - s];
         }
+        ++cnt[addr];
+    }
 
     std::cout << ret;
 }
@@ -86,14 +67,14 @@ int main(int, char**)
     std::cin.tie(nullptr);
     std::cout.tie(nullptr);
 
-    for (int64_t n = 0, s = 0; s < MAXSUM; ++n, s = n * n)
-        g_squares.insert(s);
+    g_squares.reserve(int(std::sqrt(MAXSUM) + 1));
+    for (int n = 0, s = 0; s < MAXSUM; ++n, s = n * n)
+        g_squares.push_back(s);
 
     int no_of_cases;
     std::cin >> no_of_cases;
     for (int g = 1; g <= no_of_cases; ++g) {
         std::cout << "Case #" << g << ": "; solve(); std::cout << '\n';
-        //std::cout << "Case #" << g << ": " << std::fixed << std::setprecision(9); solve(); std::cout << '\n';
     }
 }
 
