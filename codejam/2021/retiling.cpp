63a38e52bcd04ca0f073f9ce0af863872717182b
Marcin Buchwald <marcin.buchwald@outlook.com>
2021-07-09 14:59:09 +0200

    [ADD] Retiling the dead end

--- codejam/2021/retiling.cpp
+++ codejam/2021/retiling.cpp
@@ -1,63 +1,14 @@
 
 #include <algorithm>
-#include <array>
-#include <assert.h>
-#include <bitset>
-#include <cmath>
-#include <iomanip>
 #include <iostream>
-#include <iterator>
-#include <filesystem>
-#include <fstream>
-#include <functional>
-#include <limits>
-#include <map>
-#include <memory>
-#include <numeric>
-#include <optional>
-#include <queue>
-#include <random>
 #include <set>
-#include <stdlib.h>
 #include <string>
-#include <string_view>
-#include <unordered_map>
-#include <unordered_set>
-#include <tuple>
-#include <utility>
 #include <vector>
 
 // Retiling
 // https://codingcompetitions.withgoogle.com/codejam/round/0000000000435915/00000000007dc2de
 
-const auto big64 = std::numeric_limits<int64_t>::max();
-template <typename T> using vec = std::vector<T>;
-template <typename T> using vec2 = std::vector<std::vector<T>>;
-std::ofstream g_debug("retiling.log");
-vec<int64_t> g_input, g_partial;
-int64_t g_rows, g_cols, g_size;
-vec<bool> g_marked;
-
-const auto col = [](const int64_t pos) noexcept { return pos % g_cols; };
-const auto row = [](const int64_t pos) noexcept { return pos / g_cols; };
-const auto rc  = [](const int64_t row, const int64_t col) noexcept { return row * g_cols + col; };
-template <typename T> std::vector<T> fill(const size_t size){std::vector<T> cont(size);std::copy_n(std::istream_iterator<T>{std::cin},size,cont.begin());return cont;};
-template <typename T> void fill2(std::vector<std::vector<T>>& cont, int& rows, int& cols){std::cin>>rows>>cols;cont.resize(rows);for(auto& r:cont) r=fill<T>(cols);};
-template <typename T> void fill2sq(std::vector<std::vector<T>>& cont, int& size){std::cin>>size;cont.resize(size);for(auto& r:cont) r=fill<T>(size);};
-template <typename K, typename V> std::map<K, V> fillmap(size_t size){std::map<K, V> cont;for(int i=0;i<size;++i){int k,v;std::cin>>k>>v;cont[k]=v;}return cont;};
-template <typename T> T init2(const int rows, const int cols){T cont(rows,typename T::value_type(cols));return cont;};
-const auto dig_cnt = [](const int64_t n){return int(std::log10(n)) + 1;};
-const auto bin_coeff = [](const int n, const int k){return (int)(1/((n+1)*std::beta(n-k+1,k+1)));};
-const auto rand_in_range = [](const int64_t ubound){std::random_device seed;std::mt19937 gen{seed()};std::uniform_int_distribution<int64_t> dist(0,ubound-1);return dist(gen);};
-size_t next_max(const int off) {return std::lower_bound(g_input.begin()+off+1,g_input.end(),0,[](const auto& n,auto){const auto pred=&n-1;return *pred<=n;})-g_input.begin()-1;}
-size_t next_min(const int off) {return std::lower_bound(g_input.begin()+off+1,g_input.end(),0,[](const auto& n,auto){const auto pred=&n-1;return *pred>=n;})-g_input.begin()-1;}
-void partial_init(const vec<int64_t>& src){const int size=src.size();g_partial.assign(size,0);for(int i=0;i<size;++i)for(int a=i;a<size;a|=a+1)g_partial[a]+=src[i];}
-void partial_update(vec<int64_t>& src, const size_t pos, const int64_t val){const auto size=src.size();for(auto i=pos;i<size;i|=i+1)g_partial[i]+=val-src[pos];src[pos]=val;}
-int64_t partial_sum(const int i, const int j){int64_t ret{0};for(int k=j+1;k>0;k&=k-1)ret+=g_partial[k-1];for(int k=i;k>0;k&=k-1)ret-=g_partial[k-1];return ret;}
-
-
-
-static void solve() {
+static void solve_set1_incorrect() { // need maximum matching in bipartite graph for this
     int R, C, F, S; std::cin >> R >> C >> F >> S;
     std::vector<std::string> start(R), final(R);
     for (auto& r : start)
@@ -65,30 +16,33 @@ static void solve() {
     for (auto& r : final)
         std::cin >> r;
 
-    int flips{0}, swapsh{0}, swapsv{0}, swapshv{0};
-    std::unordered_set<int> tailsh, tailsv;
+    int flips{0}, swapsh{0}, swapsv{0};
+    std::set<int> tailsh, tailsv;
     for (int r = 0; r < R; ++r)
         for (int c = 0; c < C; ++c) {
-            bool flip{}, swaph{}, swapv{};
-            if (start[r][c] != final[r][c])
-                flip = true;
-            if (flip && c < C - 1 && tailsh.count(r * C + c) == 0 && start[r][c] != start[r][c + 1] && final[r][c] != final[r][c + 1]) {
-                tailsh.insert(r * C + c + 1);
-                swaph = true;
+            if (start[r][c] == final[r][c])
+                continue;
+            bool swaph{}, swapv{};
+            const int pos = r * C + c;
+            if (tailsh.count(pos) == 0 && tailsv.count(pos) == 0) {
+                if (c < C - 1 && tailsv.count(pos + 1) == 0 && start[r][c] != start[r][c + 1] && final[r][c] != final[r][c + 1]) {
+                    tailsh.insert(pos + 1);
+                    swaph = true;
+                    ++swapsh;
+                } else if (r < R - 1 && tailsh.count(pos + C) == 0 && start[r][c] != start[r + 1][c] && final[r][c] != final[r + 1][c]) {
+                    tailsv.insert(pos + C);
+                    swapv = true;
+                    ++swapsv;
+                }
             }
-            if (flip && r < R - 1 && tailsv.count(r * C + c) == 0 && start[r][c] != start[r + 1][c] && final[r][c] != final[r + 1][c]) {
-                tailsv.insert((r + 1) * C + c);
-                swapv = true;
-            }
-            if (flip) ++flips;
-            if (swaph) ++swapsh;
-            if (swapv) ++swapsv;
-            if (swaph && swapsv) ++swapshv;
+            ++flips;
         }
 
-    const int swaps = swapsh + swapsv - swapshv;
-    //std::cout << std::min(flips * F, swaps * S + (flips - 2 * swaps) * F);
-    std::cout << swaps + (flips - 2 * swaps);
+    std::vector<int> tails;
+    std::set_intersection(tailsh.begin(), tailsh.end(), tailsv.begin(), tailsv.end(), std::back_inserter(tails));
+
+    const int swaps = std::min(swapsh + swapsv - int(tails.size()), flips / 2);
+    std::cout << flips - swaps;
 }
 
 int main(int, char**)
@@ -100,7 +54,7 @@ int main(int, char**)
     int no_of_cases;
     std::cin >> no_of_cases;
     for (int g = 1; g <= no_of_cases; ++g) {
-        std::cout << "Case #" << g << ": "; solve(); std::cout << '\n';
+        std::cout << "Case #" << g << ": "; solve_set1_incorrect(); std::cout << '\n';
     }
 }
 
