ede9a9ca982296c978c2a3c141a4564e85b3f190
Marcin Buchwald <marcin.buchwald@outlook.com>
2021-07-13 20:59:02 +0200

    [FIX] Retiling Set1 solution

--- codejam/2021/retiling.cpp
+++ codejam/2021/retiling.cpp
@@ -8,6 +8,76 @@
 // Retiling
 // https://codingcompetitions.withgoogle.com/codejam/round/0000000000435915/00000000007dc2de
 
+using bipartite_t = std::vector<std::vector<bool>>;
+
+static bool bpm(const bipartite_t& bg, const int candidate, std::vector<int>& matched, std::vector<bool>& seen) {
+    const int match_size = int(bg[0].size());
+    for (int m = 0; m < match_size; ++m)
+        if (bg[candidate][m] && !seen[m]) {
+            seen[m] = true;
+            if (matched[m] < 0 || bpm(bg, matched[m], matched, seen)) {
+                matched[m] = candidate;
+                return true;
+            }
+        }
+    return false;
+}
+
+static int max_matching(const bipartite_t& bg) {
+    if (bg.empty() || bg[0].empty())
+        return 0;
+    std::vector<int> matched(bg[0].size(), -1);
+    int ret{0};
+    for (int i = 0; i < int(bg.size()); ++i) {
+        std::vector<bool> seen(matched.size());
+        if (bpm(bg, i, matched, seen))
+            ++ret;
+    }
+    return ret;
+}
+
+static void solve_set1() { // need maximum matching in bipartite graph for this
+    int R, C, F, S; std::cin >> R >> C >> F >> S;
+    std::vector<std::string> start(R), final(R);
+    for (auto& r : start)
+        std::cin >> r;
+    for (auto& r : final)
+        std::cin >> r;
+
+    // construct bipartite graph
+    int M{0}, G{0}, flips{0};
+    std::vector<std::vector<int>> order(R, std::vector<int>(C));
+    for (int r = 0; r < R; ++r)
+        for (int c = 0; c < C; ++c)
+            if (start[r][c] == 'M') 
+                order[r][c] = M++;
+            else
+                order[r][c] = G++;
+    bipartite_t bg(M, std::vector<bool>(G)); // M -> G
+    for (int r = 0; r < R; ++r)
+        for (int c = 0; c < C; ++c) {
+            if (start[r][c] == final[r][c])
+                continue;
+            if (c < C - 1 && start[r][c] != start[r][c + 1] && final[r][c] != final[r][c + 1]) {
+                if (start[r][c] == 'M')
+                    bg[order[r][c]][order[r][c + 1]] = true;
+                else
+                    bg[order[r][c + 1]][order[r][c]] = true;
+            }
+            if (r < R - 1 && start[r][c] != start[r + 1][c] && final[r][c] != final[r + 1][c]) {
+                if (start[r][c] == 'M')
+                    bg[order[r][c]][order[r + 1][c]] = true;
+                else
+                    bg[order[r + 1][c]][order[r][c]] = true;
+            }
+            ++flips;
+        }
+
+    int swaps = max_matching(bg);
+    swaps = std::min(swaps, flips / 2);
+    std::cout << flips - swaps;
+}
+
 static void solve_set1_incorrect() { // need maximum matching in bipartite graph for this
     int R, C, F, S; std::cin >> R >> C >> F >> S;
     std::vector<std::string> start(R), final(R);
@@ -22,16 +92,13 @@ static void solve_set1_incorrect() { // need maximum matching in bipartite graph
         for (int c = 0; c < C; ++c) {
             if (start[r][c] == final[r][c])
                 continue;
-            bool swaph{}, swapv{};
             const int pos = r * C + c;
             if (tailsh.count(pos) == 0 && tailsv.count(pos) == 0) {
                 if (c < C - 1 && tailsv.count(pos + 1) == 0 && start[r][c] != start[r][c + 1] && final[r][c] != final[r][c + 1]) {
                     tailsh.insert(pos + 1);
-                    swaph = true;
                     ++swapsh;
                 } else if (r < R - 1 && tailsh.count(pos + C) == 0 && start[r][c] != start[r + 1][c] && final[r][c] != final[r + 1][c]) {
                     tailsv.insert(pos + C);
-                    swapv = true;
                     ++swapsv;
                 }
             }
@@ -54,7 +121,7 @@ int main(int, char**)
     int no_of_cases;
     std::cin >> no_of_cases;
     for (int g = 1; g <= no_of_cases; ++g) {
-        std::cout << "Case #" << g << ": "; solve_set1_incorrect(); std::cout << '\n';
+        std::cout << "Case #" << g << ": "; solve(); std::cout << '\n';
     }
 }
 
