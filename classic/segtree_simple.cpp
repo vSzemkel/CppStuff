84d3311d43e5c78ea616a3cddec4179ae6ba7082
Marcin Buchwald <marcin.buchwald@outlook.com>
2021-07-21 10:45:01 +0200

    [FIX] Simple segment tree

--- classic/segtree_simple.cpp
+++ classic/segtree_simple.cpp
@@ -9,33 +9,48 @@
 
 // Find maximal element on arbitrary subrange
 
-struct maxelem_segment_tree
+template <typename T = int64_t>
+struct simple_segment_tree_t
 {
   public:
-    maxelem_segment_tree(const std::vector<int>& input) {
+    simple_segment_tree_t(const std::vector<T>& input) {
         const int input_size = (int)input.size();
         while (offset < input_size) offset *= 2;
-        nodes.assign(2 * offset, MIN32);
+        nodes.resize(2 * offset);
         std::copy(input.begin(), input.end(), nodes.begin() + offset);
         for (int n = offset - 1; n > 0; --n)
-            nodes[n] = std::max(nodes[2 * n], nodes[2 * n + 1]);
+            nodes[n] = join(nodes[2 * n], nodes[2 * n + 1]);
     }
 
-    int query(int lower, int upper) const { // range [lower, upper)
-        int ret{MIN32};
+    T query(int lower, int upper) const { // range [lower, upper)
+        T ret{};
         for (lower += offset, upper += offset; lower < upper; lower >>= 1, upper >>= 1) { 
             if (lower & 1)
-                ret = std::max(ret, nodes[lower++]); 
+                ret = join(ret, nodes[lower++]); 
             if (upper & 1)
-                ret = std::max(ret, nodes[--upper]);
+                ret = join(ret, nodes[--upper]);
         }
 
         return ret;
     }
 
+    void update(int pos, const T val) {
+        pos += offset;
+        nodes[pos] = val;
+        while (pos > 1) {
+            pos /= 2;
+            nodes[pos] = join(nodes[2 * pos], nodes[2 * pos + 1]);
+        }
+    }
+
     static const int MIN32 = std::numeric_limits<int>::min();
+
   private:
-    std::vector<int> nodes;
+    static T join(const T& lhs, const T& rhs) {
+        return std::max(lhs, rhs); // TO DO: change join implementation
+    }
+
+    std::vector<T> nodes;
     int offset{1};
 };
 
@@ -50,12 +65,20 @@ void fuzz()
     for (int i = 0; i < size; ++i)
         input[i] = rand_in_range(max);
 
-    maxelem_segment_tree mst{input};
+    simple_segment_tree_t<int> mst{input};
     for (int t = 0; t < trials; ++t) {
         int lower = rand_in_range(size);
         int upper = rand_in_range(size);
         if (lower > upper) std::swap(lower, upper);
-        int ret{maxelem_segment_tree::MIN32};
+
+        int ret{simple_segment_tree_t<>::MIN32};
+        for (int i = lower; i < upper; ++i)
+            ret = std::max(ret, input[i]);
+        assert(ret == mst.query(lower, upper));
+
+        const int pos = rand_in_range(size);
+        const int val = rand_in_range(max);
+        mst.update(pos, val);
         for (int i = lower; i < upper; ++i)
             ret = std::max(ret, input[i]);
         assert(ret == mst.query(lower, upper));
@@ -68,7 +91,7 @@ int main(int, char**)
     std::cin.tie(nullptr);
     std::cout.tie(nullptr);
 
-    maxelem_segment_tree mst = std::vector<int>{1, 15, 3, 7, 11};
+    simple_segment_tree_t<int> mst = std::vector<int>{1, 15, 3, 7, 11};
     assert(mst.query(0, 5) == 15);
     assert(mst.query(1, 4) == 15);
     assert(mst.query(2, 5) == 11);
@@ -84,17 +107,10 @@ int main(int, char**)
 /*
 
 Compile:
-clang++.exe -Wall -Wextra -ggdb3 -O0 -std=c++17 segment_tree.cpp -o segment_tree.exe
-g++ -Wall -Wextra -ggdb3 -Og -std=c++17 segment_tree.cpp -o segment_tree.o
+clang++.exe -Wall -Wextra -ggdb3 -O0 -std=c++17 segtree_simple.cpp -o segtree_simple.exe
+g++ -Wall -Wextra -ggdb3 -Og -std=c++17 segtree_simple.cpp -o segtree_simple.o
 
 Run:
-py.exe interactive_runner.py py.exe segment_tree_testing_tool.py 1 -- segment_tree.exe
-segment_tree.exe < segment_tree.in
-
-Input:
-
-
-Output:
-
+segtree_simple.exe
 
 */
