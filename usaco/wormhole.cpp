fc5421ca9b1d1fa5a9b12980d88479947b505fd1
Marcin Buchwald <marcin.buchwald@outlook.com>
2021-08-20 11:50:29 +0200

    [FIX] Wormhole 7/10

--- usaco/wormhole.cpp
+++ usaco/wormhole.cpp
@@ -6,30 +6,11 @@ PROBLEM STATEMENT: https://train.usaco.org/usacoprob2?a=Re1euIbhzhZ&S=wormhole
 */
 
 #include <algorithm>
-#include <array>
 #include <assert.h>
-#include <bitset>
 #include <cmath>
-#include <iomanip>
-#include <iostream>
-#include <iterator>
-#include <filesystem>
 #include <fstream>
-#include <functional>
-#include <limits>
-#include <map>
-#include <memory>
+#include <iostream>
 #include <numeric>
-#include <optional>
-#include <queue>
-#include <random>
-#include <set>
-#include <stdlib.h>
-#include <string>
-#include <string_view>
-#include <unordered_map>
-#include <unordered_set>
-#include <tuple>
 #include <utility>
 #include <vector>
 
@@ -37,8 +18,7 @@ std::ifstream task_in("wormhole.in");
 std::ofstream task_out("wormhole.out");
 
 std::vector<bool> visited;
-std::vector<std::pair<int, int>> points;
-std::vector<int> pairing, reachable, wormable;
+std::vector<int> reachable, wormable;
 
 static double factorial(const int n) {
     return std::tgamma(n + 1);
@@ -47,7 +27,7 @@ static double factorial(const int n) {
 static bool has_cycle(const int start, const int target, const bool use_wormhole) {
     const int dest = use_wormhole ? wormable[start] : reachable[start];
     if (dest >= 0) {
-        if (dest == target)
+        if (use_wormhole && dest == target)
             return true;
         if (!visited[dest]) {
             visited[dest] = true;
@@ -59,22 +39,17 @@ static bool has_cycle(const int start, const int target, const bool use_wormhole
     return false;
 }
 
-static std::pair<int, int> get_pair(const int p) { // returns indexes of points in pair p
-    const auto first = std::find(pairing.begin(), pairing.end(), p) - pairing.begin();
-    const auto second = std::find(pairing.begin() + first + 1, pairing.end(), p) - pairing.begin();
-    return {first, second};
-}
-
 int main(int, char**)
 {
     int N; task_in >> N;
     assert((N & 1) == 0);
     const int P = N / 2;
-    points.resize(N);
-    pairing.resize(N);
+    // const int MAX = factorial(N) / (1 << P);
+    std::vector<std::pair<int, int>> points(N), pair_indexes;
     wormable.resize(N);
     for (auto& p : points)
         task_in >> p.first >> p.second;
+    std::vector<int> pairing(N);
     for (int i = 0; i < N; i += 2)
         pairing[i] = pairing[i + 1] = i / 2;
     std::sort(points.begin(), points.end(), [](const auto& p1, const auto& p2){
@@ -88,30 +63,34 @@ int main(int, char**)
 
     int ret{0};
     do {
-        std::vector<std::pair<int, int>> pair_indexes(P);
-        for (int p = 0; p < P; ++p) {
-            const auto& w = pair_indexes[p] = get_pair(p);
-            wormable[w.first] = w.second;
-            wormable[w.second] = w.first;
+        pair_indexes.assign(P, {-1, -1});
+        for (int i = 0; i < N; ++i) {
+            auto& pair = pair_indexes[pairing[i]];
+            if (pair.first == -1) {
+                pair.first = i;
+            } else {
+                pair.second = i;
+                wormable[pair.first] = i;
+                wormable[i] = pair.first;
+            }
         }
 
-        for (auto p : pairing) std::cout << p << ' '; 
-        std::cout << '\n';
-        for (const auto& w : pair_indexes) {
-            visited.assign(N, false);
-            if (has_cycle(w.first, w.first, false)) {
-                ++ret;
-                break;
+        if (std::is_sorted(pair_indexes.begin(), pair_indexes.end()))
+            for (const auto& w : pair_indexes) {
+                visited.assign(N, false);
+                if (has_cycle(w.first, w.first, false)) {
+                    ++ret;
+                    break;
+                }
+                visited.assign(N, false);
+                if (has_cycle(w.second, w.second, false)) {
+                    ++ret;
+                    break;
+                }
             }
-            visited.assign(N, false);
-            if (has_cycle(w.second, w.second, false)) {
-                ++ret;
-                break;
-            }
-        }
     } while (std::next_permutation(pairing.begin(), pairing.end()));
 
-    task_out << ret / int(factorial(P)) << '\n';
+    task_out << ret << '\n';
 }
 
 /*
