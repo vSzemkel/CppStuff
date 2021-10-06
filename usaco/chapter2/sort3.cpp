a1c6c316f1d5945a88aacea75c145fa590bd2a91
Marcin Buchwald <marcin.buchwald@outlook.com>
2021-10-06 21:34:47 +0200

    [FIX] sort3 with std::span

--- usaco/chapter2/sort3.cpp
+++ usaco/chapter2/sort3.cpp
@@ -53,28 +53,29 @@ int main(int, char**)
 {
     task_in >> N;
     std::vector<int> data(N);
-    std::array<std::vector<int>, 4> positions;
+    std::array<int, 4> counts{};
     std::array<std::pair<int, int>, 4> ranges;
     for (int i = 0; i < N; ++i) {
         int val; task_in >> val;
         data[i] = val;
-        positions[val].push_back(i);
+        ++counts[val];
     }
-    ranges[1] = {0, int(positions[1].size())};
-    ranges[2] = {ranges[1].second, ranges[1].second + int(positions[2].size())};
+    ranges[1] = {0, counts[1]};
+    ranges[2] = {counts[1], counts[1] + counts[2]};
     ranges[3] = {ranges[2].second, N};
 
     // swap all pairs with two correcting positions
+    // altervative brilliant idea - sort data_copy and swap if (d[i]!=c[i] && d[j]!=c[j] && d[i]==c[j] && d[j]==c[i])
     for (int val : {1, 2})
-        for (int i = ranges[val].first; i < ranges[val].second; ++i)
-            if (data[i] != val) {
-                const auto& range = ranges[data[i]];
-                for (int j : positions[val])
-                    if (range.first <= j && j < range.second && data[j] == val) {
+        for (int i = ranges[val].first; i < ranges[val].second; ++i) {
+            const int mis = data[i];
+            if (mis != val)
+                for (int j = ranges[mis].first; j < ranges[mis].second; ++j)
+                    if (data[j] == val) {
                         swap(data, i, j);
                         break;
                     }
-            }
+        }
 
     // reminders are 3-cycles, each needs 2 swaps to correct
     int misplaced{0};
@@ -93,6 +94,36 @@ int main(int, char**)
 }
 
 /*
+
+#include <span>
+int main(int, char**)
+{
+    task_in >> N;
+    std::vector<int> data(N);
+    std::array<size_t, 4> counts{};
+    std::array<std::span<int>, 4> ranges;
+    for (int i = 0; i < N; ++i) {
+        int val; task_in >> val;
+        data[i] = val;
+        ++counts[val];
+    }
+    ranges[1] = {data.begin(), counts[1]};
+    ranges[2] = {data.begin() + counts[1], counts[2]};
+    ranges[3] = {data.begin() + counts[1] + counts[2], counts[3]};
+
+    // swap all pairs with two correcting positions
+    // altervative brilliant idea - sort data_copy and swap if (d[i]!=c[i] && d[j]!=c[j] && d[i]==c[j] && d[j]==c[i])
+    for (int val : {1, 2})
+        for (int& i : ranges[val]) {
+            if (i != val)
+                for (int& j : ranges[i])
+                    if (j == val) {
+                        std::swap(i, j);
+                        ++ans;
+                        break;
+                    }
+        }
+
 
 Compile:
 clang++.exe -Wall -Wextra -ggdb3 -O0 -std=c++17 sort3.cpp -o sort3.exe
