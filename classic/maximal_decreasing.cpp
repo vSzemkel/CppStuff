06e34d14bdf98e4d1ed21de6af24690875dd39c5
Marcin Buchwald <marcin.buchwald@outlook.com>
2021-11-03 10:47:33 +0100

    [ADD] Maximal decreasing revisited

--- classic/maximal_decreasing.cpp
+++ classic/maximal_decreasing.cpp
@@ -21,10 +21,42 @@ static void print(const C& v)
     std::cout << '\n';
 }
 
-static void solve(const std::vector<int>& input) { // O(NlogN)
+static std::vector<int> solve(const std::vector<int>& input) { // O(NlogA) with O(N) memory
     assert(!input.empty());
-    print(input);
+    const int size = int(input.size());
+    std::vector<int> tails; // tails[K] = X iff exists decreasing sequence of length K+1 ending with X
+    std::vector<int> locations; // locations[K] = X iff exists decreasing sequence of length K+1 ending with input[X]
+    std::vector<int> prev(size, -1); // prev[K] = X iff exists decreasing sequence of length K+1 ending with [input[X], tails[K]]
 
+    locations.push_back(0);
+    tails.push_back(input.front());
+    for (int i = 1; i < size; ++i) {
+        const auto val = input[i];
+        if (val < tails.back()) {
+            prev[i] = locations.back();
+            tails.push_back(val);
+            locations.push_back(i);
+        } else {
+            const auto it = std::lower_bound(tails.begin(), tails.end(), val, [](const int e, const int v){
+                return v < e;
+            });
+            *it = val;
+            const int ind = it - tails.begin();
+            locations[ind] = i;
+            prev[i] = (ind > 0) ? locations[ind - 1] : -1;
+        }
+    }
+
+    std::vector<int> ans;
+    for (int p = locations.back(); p >= 0; p = prev[p])
+        ans.push_back(input[p]);
+    std::reverse(ans.begin(), ans.end());
+
+    return ans;
+}
+
+static std::vector<int> solve2(const std::vector<int>& input) { // O(NlogA) with O(A2) memory
+    assert(!input.empty());
     std::vector<std::vector<int>> seqs; // seqs[K] is optimal so far decreasing sequence ot length K+1
     std::vector<int> scores; // scores[K] = X iff exists decreasing sequence of length K+1 ending with X
 
@@ -51,9 +83,7 @@ static void solve(const std::vector<int>& input) { // O(NlogN)
         }
     }
 
-    std::cout << "Maximal decreasing subsequence length is: " << scores.size() << '\n';
-    print(seqs.back());
-    std::cout << std::endl;
+    return seqs.back();
 }
 
 constexpr int size = 35;
@@ -61,10 +91,14 @@ constexpr int range = 70;
 
 int main(int, char**)
 {
-    solve({10, 8, 9, 4, 6, 3});
     std::vector<int> input;
     std::generate_n(std::back_inserter(input), size, []{ return rand_in_range(range); });
-    solve(input);
+    print(input);
+
+    const auto ans = solve(input);
+    std::cout << "Maximal decreasing subsequence length is: " << ans.size() << '\n';
+    print(ans);
+    assert(ans == solve2(input));
 }
 
 /*
