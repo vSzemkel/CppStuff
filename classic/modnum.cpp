764360d665082536246b15f68686ca8712122b96
Marcin Buchwald <marcin.buchwald@outlook.com>
2021-12-11 10:24:27 +0100

    [ADD] modenum_t with templated value type

--- classic/modnum.cpp
+++ classic/modnum.cpp
@@ -5,15 +5,15 @@
 
 // Modnum - integral datatype implementing modulo arithmetic
 
-template <int M = 998244353>
+template <typename T = int, T M = 998244353>
 class modnum_t {
   public:
     modnum_t() : value(0) {}
-    modnum_t(int64_t v) : value(v % M) {
+    modnum_t(T v) : value(v % M) {
         if (value < 0) value += M;
     }
 
-    modnum_t inv() const
+    modnum_t inv() const // not exists if < 0
     {
         modnum_t res;
         res.value = minv(value, M);
@@ -22,9 +22,7 @@ class modnum_t {
 
     modnum_t neg() const
     {
-        modnum_t res;
-        res.value = value ? M - value : 0;
-        return res;
+        return modnum_t{value ? M - value : 0};
     }
 
     modnum_t operator-() const
@@ -53,7 +51,7 @@ class modnum_t {
 
     modnum_t& operator*=(const modnum_t& o)
     {
-        value = int(int64_t(value) * int64_t(o.value) % M);
+        value = modmul(value, o.value);
         return *this;
     }
 
@@ -93,8 +91,8 @@ class modnum_t {
     friend std::optional<modnum_t> inv(const modnum_t& m) { const auto ret = m.inv(); return ret.value < 0 ? std::nullopt : std::optional{ret}; }
     friend modnum_t neg(const modnum_t& m) { return m.neg(); }
 
-    friend std::ostream& operator<<(std::ostream& out, const modnum_t& n) { return out << int(n); }
-    friend std::istream& operator>>(std::istream& in, modnum_t& n) { int64_t v; in >> v; n = modnum_t(v); return in; }
+    friend std::ostream& operator<<(std::ostream& out, const modnum_t& n) { return out << T(n); }
+    friend std::istream& operator>>(std::istream& in, modnum_t& n) { T v; in >> v; n = modnum_t(v); return in; }
 
     friend bool operator<(const modnum_t& a, const modnum_t& b) { return a.value < b.value; }
     friend bool operator==(const modnum_t& a, const modnum_t& b) { return a.value == b.value; }
@@ -103,31 +101,64 @@ class modnum_t {
     friend modnum_t operator-(const modnum_t& a, const modnum_t& b) { return modnum_t(a) -= b; }
     friend modnum_t operator*(const modnum_t& a, const modnum_t& b) { return modnum_t(a) *= b; }
     friend modnum_t operator/(const modnum_t& a, const modnum_t& b) { return modnum_t(a) /= b; }
-    friend int operator%(const modnum_t& a, const int b) { return a.value % b; }
+    friend T operator%(const modnum_t& a, const T b) { return a.value % b; }
 
-    explicit operator int() const { return value; }
+    explicit operator int() const { return int(value); }
+    explicit operator int64_t() const { return int64_t(value); }
 
-  private:
-    static int minv(int a, const int m) {
-        a %= m;
-        if (a == 0) return -1; // not exists
-        return a == 1 ? 1 : int(m - int64_t(minv(m, a)) * int64_t(m) / a);
+    static modnum_t bin_coeff(const int n, const int k)
+    {
+        modnum_t ret{1};
+        for (int i = 0; i < k; ++i)
+            ret = (ret * (n - i)) / (i + 1);
+        return ret;
     }
 
-    int value;
+    static T add(T a, T b)
+    {
+        a += b;
+        while (a >= M)
+            a -= M;
+        return a;
+    }
+
+    static T modmul(T a, T b)
+    {
+        int64_t r{0};
+        while (b) {
+            if (b & 1) r = add(r, a);
+            b >>= 1;
+            a = add(a, a);
+        }
+        return r;
+    }
+
+  private:
+    static T minv(T a, const T m) {
+        a %= m;
+        if (a == 0) return -1; // not exists
+        return a == 1 ? 1 : T(m - int64_t(minv(m, a)) * int64_t(m) / a);
+    }
+
+    T value;
 };
 
 int main(int, char**)
 {
-    modnum_t<20> m = 13;
-    assert((int)inv(m).value() == 17); // 13 * 17 == 1 (mod 20)
-    modnum_t<20> m2 = 15;
+    modnum_t<int, 20> m = 13;
+    assert(inv(m).value() == 17); // 13 * 17 == 1 (mod 20)
+    modnum_t<int64_t, 20> m2 = 15;
     assert(!inv(m2).has_value());
 
-    assert((int)neg(m2) == 5);
+    assert(neg(m2) == 5);
     auto m3 = m2++;
-    assert((int)m2 == 16 && (int)m3 == 15);
-    assert((int)(m2 * m3) == 0);
+    assert(m2 == 16 && m3 == 15);
+    assert(m2 * m3 == 0);
+
+    modnum_t<int64_t> m4;
+    assert(m4.modmul(16351848, 9174561) == 872592476);
+    assert(m4.bin_coeff(197, 8) == 26874261); // 48717346033720 % 998244353
+    std::cout << "PASSED\n";
 }
 
 /*
