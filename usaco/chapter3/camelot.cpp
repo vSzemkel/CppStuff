1f8041076bb168ef60d6a19527c351d6a3922573
Marcin Buchwald <marcin.buchwald@outlook.com>
2022-01-27 17:42:21 +0100

    [ADD] Knight distance with dijkstra

--- usaco/chapter3/camelot.cpp
+++ usaco/chapter3/camelot.cpp
@@ -10,138 +10,108 @@ PROBLEM STATEMENT: https://train.usaco.org/usacoprob2?a=unnUOe16hgm&S=camelot
 #include <assert.h>
 #include <fstream>
 #include <numeric>
+#include <queue>
 #include <vector>
 
 std::ifstream task_in("camelot.in");
 std::ofstream task_out("camelot.out");
 
-struct pos_t : std::array<int, 2> {
-    pos_t diff(const pos_t& other) const {
-        pos_t k; k[0] = std::abs((*this)[0] - other[0]); k[1] = std::abs((*this)[1] - other[1]);
-        return k;
-        //return pos_t{std::abs((*this)[0] - other[0]), std::abs((*this)[1] - other[1])};
-    }
-};
+constexpr int INF = 1e09;
 
-constexpr int kd[6] = {0, 3, 2, 3, 2, 1}; // 3 * r + c
+std::vector<int> knight_distance;
+std::vector<bool> in_queue;
+std::queue<int> qq;
+
+using pos_t = std::pair<int, int>;
+
+const pos_t singularity{1, 1};
+
+static pos_t diff(const pos_t& pos, const int r, const int c) {
+    return {std::abs(pos.first - r), std::abs(pos.second - c)};
+}
 
 /**
- * @brief Move knight towards origin
+ * @brief Precompute knight distances between every two cells on board
  * 
- * @param out r closest position in 2x3 surroundings
- * @param out c closest position in 2x3 surroundings
- * @return int Number of moves
+ * @param rows Board's height
+ * @param cols Board's width
  */
-static int distance(int& r, int& c) {
-    int ret{0};
-    while (r + c > 3) {
-        if (r > c) {
-            r -= 2;
-            c ? --c : c = 1;
-        } else {
-            c -= 2;
-            r ? --r : r = 1;
+static void init_knight_distance(const int rows, const int cols) {
+    const auto sz = rows * cols;
+    knight_distance.assign(sz, INF);
+    in_queue.assign(sz, false);
+
+    const auto dijkstra = [](const int cost, const int next) {
+        if (cost < knight_distance[next]) {
+            knight_distance[next] = cost;
+            if (!in_queue[next]) {
+                in_queue[next] = true;
+                qq.push(next);
+            }
         }
-        ++ret;
+    };
+
+    knight_distance[0] = 0;
+    qq.push(0);
+    in_queue[0] = true;
+    while (!qq.empty()) {
+        const int cell = qq.front(); qq.pop();
+        in_queue[0] = false;
+        const int r = cell / cols;
+        const int c = cell % cols;
+        const int cost = knight_distance[r * cols + c] + 1;
+        if (c + 2 < cols) {
+            if (r > 0) dijkstra(cost, (r - 1) * cols + c + 2);
+            if (r < rows - 1) dijkstra(cost, (r + 1) * cols + c + 2);
+        }
+        if (0 <= c - 2) {
+            if (r > 0) dijkstra(cost, (r - 1) * cols + c - 2);
+            if (r < rows - 1) dijkstra(cost, (r + 1) * cols + c - 2);
+        }
+        if (r + 2 < rows) {
+            if (c > 0) dijkstra(cost, (r + 2) * cols + c - 1);
+            if (c < cols - 1) dijkstra(cost, (r + 2) * cols + c + 1);
+        }
+        if (0 <= r - 2) {
+            if (c > 0) dijkstra(cost, (r - 2) * cols + c - 1);
+            if (c < cols - 1) dijkstra(cost, (r - 2) * cols + c + 1);
+        }
     }
-
-    if (r < 0) r = 1;
-    if (c < 0) c = 1;
-    if (r == 3 || c == 3)
-        r = c = 1;
-
-    return ret;
-}
-
-static int knight_distance(int r, int c) {
-    const int ret = distance(r, c);
-    if (r <= c)
-        return ret + kd[3 * r + c];
-
-    return ret + kd[3 * c + r];
-
-    /*if (c == 0) {
-        if (r == 0) return 0;
-        if (r == 1) return 3;
-    } else if (c == 1) {
-        if (r == 0) return 3;
-        if (r == 1) return 2;
-    } else { // c == 2
-        if (r == 0) return 2;
-        if (r == 1) return 1;
-    }
-
-    assert(knight_distance(6, 5) == 5);
-    assert(knight_distance(5, 5) == 6);
-    assert(knight_distance(5, 4) == 3);
-    assert(knight_distance(5, 3) == 4);
-    assert(knight_distance(4, 4) == 4);*/
-}
-
-/**
- * @brief Let the knight meet the king in minimal numer of moves
- * @param knight Knights position is set to meetpoint
- * @return int Number of moves
- */
-static int king_knight_meeting(const pos_t& king, pos_t& knight) {
-    const auto d = king.diff(knight);
-    int r{d[0]}, c{d[1]};
-    int ret = distance(r, c);
-    const int sigr = r ? (knight[0] - king[0]) / r : 0;
-    const int sigc = c ? (knight[1] - king[1]) / c : 0;
-
-    knight[0] = king[0] + r * sigr;
-    knight[1] = king[1] + c * sigc;
-
-    return ret + std::max(r, c);
-
-    /*pos_t king2{5, 5}, knight{8, 9};
-    assert((king_knight_meeting(king2, knight) == 3 && knight == pos_t{6, 5}));*/
 }
 
 int main(int, char**)
 {
     int R, C;
     task_in >> R >> C;
-    const int SZ = R * C;
     int r; char c;
-    task_in >> c >> r;
-    //pos_t king = {r - 1, c[0] - 'A'};
-    pos_t king; king[0] = r - 1; king[1] = c - 'A';
+    task_in >> c >> r >> std::ws;
+    pos_t king = {r - 1, c - 'A'};
     std::vector<pos_t> knights;
-    while (true) {
-        task_in >> c >> r;
-        pos_t k; k[0] = r - 1; k[1] = c - 'A';
-        knights.push_back(k);
-        if (!task_in.good())
-            break;
-        //knights.push_back({r - 1, c[0] - 'A'});
-    };
+    while (task_in.good()) {
+        task_in >> c >> r >> std::ws;
+        knights.emplace_back(r - 1, c - 'A');
+    }
 
     if (knights.empty()) {
         task_out << "0\n";
         return 0;
     }
 
-    // take the king
-    pos_t& driver = *std::min_element(knights.begin(), knights.end(), [&king](const pos_t& knight1, const pos_t& knight2){
-        const pos_t d1 = king.diff(knight1);
-        const pos_t d2 = king.diff(knight2);
-        return d1[0] * d1[0] + d1[1] * d1[1] < d2[0] * d2[0] + d2[1] * d2[1];
-    });
+    init_knight_distance(R, C);
 
-    int ans = 1e09;
-    int meet_king = king_knight_meeting(king, driver);
-    for (int pos = 0; pos < SZ; ++pos) {
-        int can{meet_king};
-        for (const auto& k : knights) {
-            pos_t p; p[0] = pos / C; p[1] = pos % C;
-            const auto d = k.diff(p);
-            //const auto d = k.diff(pos_t{pos / C, pos % C});
-            can += knight_distance(d[0], d[1]);
+    int ans = INF;
+    for (int r = 0; r < R; ++r)
+        for (int c = 0; c < C; ++c) {
+            int can{0};
+            for (const auto& k : knights) {
+                const auto d = diff(k, r, c);
+                if (d == singularity && !(r == 0 || c == 0 || r == R - 1 || c == C - 1 || k.first == 0 || k.second == 0 || k.first == R - 1 || k.second == C - 1))
+                    can += 2;
+                else
+                    can += knight_distance[d.first * C + d.second];
+            }
+            ans = std::min(ans, can);
         }
-        ans = std::min(ans, can);
-    }
 
     task_out << ans << '\n';
 }
