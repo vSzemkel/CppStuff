035f2d579479ffe658370528633a35097f657be7
Marcin Buchwald <marcin.buchwald@outlook.com>
2022-02-07 09:10:19 +0100

    [ADD] Rockers task from USACO fith full search

--- usaco/chapter3/rockers.cpp
+++ usaco/chapter3/rockers.cpp
@@ -5,32 +5,7 @@ TASK: rockers
 PROBLEM STATEMENT: https://train.usaco.org/usacoprob2?a=M8dKihAaeaL&S=rockers
 */
 
-#include <algorithm>
-#include <array>
-#include <assert.h>
-#include <bitset>
-#include <cmath>
-#include <iomanip>
-#include <iostream>
-#include <iterator>
-#include <filesystem>
 #include <fstream>
-#include <functional>
-#include <limits>
-#include <map>
-#include <memory>
-#include <numeric>
-#include <optional>
-#include <queue>
-#include <random>
-#include <set>
-#include <stdlib.h>
-#include <string>
-#include <string_view>
-#include <unordered_map>
-#include <unordered_set>
-#include <tuple>
-#include <utility>
 #include <vector>
 
 std::ifstream task_in("rockers.in");
@@ -38,74 +13,40 @@ std::ofstream task_out("rockers.out");
 
 int N, T, M; // num of songs, disk capacity, max number of disks
 std::vector<int> items;
-std::vector<bool> released;
-std::vector<bool> dp; // dp[i] == bool(i is a sum of subset of first processed elements)
-std::vector<std::vector<bool>> choosen; // choosen[i][j] == bool(j-th element is included in subset summing to i)
 
-/**
- * @brief Selects unreleased songs to minimize disk space waste
- * @return int Number of songs
- */
-static int subset_sum() {
-    choosen.assign(T + 1, std::vector<bool>(N + 1));
-    dp.assign(T + 1, false);
-    dp[0] = true;
-    for (int i = 0; i < N; ++i) {
-        if (released[i])
-            continue;
-        for (int t = T; t >= items[i]; --t) {
-            choosen[t][i + 1] = !dp[t] && dp[t - items[i]];
-            dp[t] = dp[t] | dp[t - items[i]];
+void full_search() { // O(2^N * N)
+    int ans{0};
+    const int SETS = 1 << N;
+    for (int sel = 1; sel < SETS; ++sel) {
+        int pos{0}, ret{0}, mask{1};
+        for (int z = M; z; --z) {
+            int sum{0};
+            while (pos < N) {
+                if (sel & mask) {
+                    if (sum + items[pos] > T)
+                        break;
+                    sum += items[pos];
+                    ++ret;
+                }
+                ++pos;
+                mask <<= 1;
+            }
         }
+        if (ans < ret)
+            ans = ret;
     }
 
-    int cap = -1;
-    for (int z = T; z; --z)
-        if (dp[z]) {
-            cap = z;
-            break;
-        }
-
-    if (~cap) {
-        int acc{0};
-        while (cap) {
-            const auto& chn = choosen[cap];
-            const int song = std::find(chn.begin(), chn.end(), true) - chn.begin() - 1;
-            released[song] = true;
-            cap -= items[song];
-            ++acc;
-        }
-        return acc;
-    }
-
-    return 0;
+    task_out << ans << '\n';
 }
 
 int main(int, char**)
 {
     task_in >> N >> T >> M;
-    released.assign(N, false);
     items.resize(N);
     for (auto& s : items)
         task_in >> s;
-    std::sort(items.begin(), items.end());
 
-    std::priority_queue<int> albums;
-    while (true) {
-        const int next_album = subset_sum();
-        if (next_album == 0)
-            break;
-        albums.push(next_album);
-    }
-
-    int ret{0};
-    for (int z = M; z; --z)
-        if (!albums.empty()) {
-            ret += albums.top();
-            albums.pop();
-        }
-
-    task_out << ret << '\n';
+    full_search();
 }
 
 /*
@@ -116,8 +57,13 @@ g++ -Wall -Wextra -ggdb3 -Og -std=c++17 -fsanitize=address rockers.cpp -o rocker
 
 Input:
 
+20 20 10 
+18 15 16 10 2 20 14 17 3 7 16 15 18 16 20 16 13 9 4 16 
+0b11110000001111111111
+18 15 16 10 2 20 14 17 3 7 *16 *15 *18 *16 *20 *16 13 9 4 16 
 
 Output:
 
+14
 
 */
