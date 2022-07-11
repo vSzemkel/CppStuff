e1468b69a225db07bc660278cf8776f83fa60869
BENTLEY\Marcin.Buchwald <marcin.buchwald@bentley.com>
2022-07-11 11:55:24 +0200

    [ADD] stopwatch helper utility

---
+++ snippets/stopwatch.cpp
@@ -0,0 +1,25 @@
+
+#include <chrono>
+#include <iostream>
+#include <thread>
+
+struct stopwatch_t {
+    stopwatch_t() { _start = std::chrono::high_resolution_clock::now(); }
+    ~stopwatch_t() {
+        const auto d =  std::chrono::high_resolution_clock::now() - _start;
+        if (d.count() > std::giga::num)
+            std::cout << std::chrono::duration<double>(d).count() << " sec.";
+        else
+            std::cout << std::chrono::duration<double, std::milli>(d).count() << " ms.";
+    }
+  private:
+     std::chrono::high_resolution_clock::time_point _start;
+};
+
+int main(int, char**)
+{
+    stopwatch_t sw;
+    using namespace std::chrono_literals;
+    std::this_thread::sleep_for(1s);
+}
+
