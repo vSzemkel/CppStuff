a1117a610d2168864644ec43574f4c8ced27fbca
Marcin Buchwald <marcin.buchwald@outlook.com>
2022-11-13 09:01:00 +0100

    [FIX] Magical Well Of Lilies from Kickstart 2022 - Round H

--- kickstart/2022/magical_well_of_lilies.cpp
+++ kickstart/2022/magical_well_of_lilies.cpp
@@ -1,12 +1,37 @@
 
+#include <array>
 #include <iostream>
 
 // Magical Well Of Lilies
 // https://codingcompetitions.withgoogle.com/kickstart/round/00000000008cb1b6/0000000000c47e79
 
-int L, ans;
+constexpr const int INF = 1e09;
+constexpr const int MAX = 1e05;
 
-static void inner_solve(int acc, int mem, int cost) { // O(3^N)
+int L, ans;
+std::array<int, MAX + 1> dp;
+
+/**
+ * @brief For each i compute one coin operation backward
+ *        and sequence of two coins operation forward
+ */
+static void precompute() {
+    dp.fill(INF);
+    dp[0] = 0;
+    dp[1] = 1;
+    for (int i = 2; i <= MAX; ++i) {
+        dp[i] = std::min(dp[i], dp[i - 1] + 1);
+        for (int j = 2 * i, c = 6; j <= MAX; j += i, c += 2)
+            dp[j] = std::min(dp[j], dp[i] + c);
+    }
+}
+
+static void solve() {
+    std::cin >> L;
+    std::cout << dp[L];
+}
+
+static void inner_solve_set1(int acc, int mem, int cost) { // O(3^N)
     if (acc >= L) {
         if (acc == L && cost < ans)
             ans = cost;
@@ -14,18 +39,18 @@ static void inner_solve(int acc, int mem, int cost) { // O(3^N)
     }
 
     if (cost + 1 < ans)
-        inner_solve(acc + 1, mem, cost + 1);
+        inner_solve_set1(acc + 1, mem, cost + 1);
     if (0 < mem && cost + 2 < ans)
-        inner_solve(acc + mem, mem, cost + 2);
+        inner_solve_set1(acc + mem, mem, cost + 2);
     if (mem < acc && cost + 4 < ans)
-        inner_solve(acc, acc, cost + 4);
+        inner_solve_set1(acc, acc, cost + 4);
 }
 
-static void solve() {
+static void solve_set1() {
     std::cin >> L;
 
     ans = 1e09;
-    inner_solve(0, 0, 0);
+    inner_solve_set1(0, 0, 0);
     std::cout << ans;
 }
 
@@ -37,6 +62,9 @@ int main(int, char**)
 
     int no_of_cases;
     std::cin >> no_of_cases;
+
+    precompute();
+
     for (int g = 1; g <= no_of_cases; ++g) {
         std::cout << "Case #" << g << ": "; solve(); std::cout << '\n';
     }
@@ -56,12 +84,12 @@ Input:
 3
 17
 18
-19
+1974
 
 Output:
 
 Case #1: 15
 Case #2: 14
-Case #3: 15
+Case #3: 50
 
 */
