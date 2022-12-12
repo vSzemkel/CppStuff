13df1a06ff376cc5c871aa9903c58967a8e89cfc
Marcin Buchwald <marcin.buchwald@outlook.com>
2022-12-12 16:50:41 +0100

    [ADD] Latest guest task from Kickstart 2019 - Round D

--- kickstart/2019/latest_guests.cpp
+++ kickstart/2019/latest_guests.cpp
@@ -1,109 +1,59 @@
 
-#include <algorithm>
-#include <array>
-#include <bitset>
-#include <cassert>
-#include <cmath>
-#include <cstdlib>
-#include <cstring>
-#include <iomanip>
 #include <iostream>
-#include <iterator>
-#include <filesystem>
-#include <fstream>
-#include <functional>
-#include <limits>
-#include <map>
-#include <memory>
-#include <numeric>
-#include <optional>
-#include <queue>
-#include <random>
-#include <set>
-#include <string>
-#include <string_view>
-#include <unordered_map>
-#include <unordered_set>
-#include <tuple>
 #include <utility>
 #include <vector>
 
 // Latest Guests
 // https://codingcompetitions.withgoogle.com/kickstart/round/0000000000051061/0000000000161427
 
-static void solve() {
+/**
+ * @brief Assume clockwise guests are moving in the right direction (we are in the center)
+ */
+static void solve() { // koodos to wifi
     int N, G, M;
     std::cin >> N >> G >> M;
+    std::vector<int> left(N, -1), right(N, -1); // directional time of last visit
     std::vector<std::pair<int, bool>> guests(G); // {position, clockwise}
     for (auto& g : guests) {
         char dir;
         std::cin >> g.first >> dir;
         --g.first;
         g.second = dir == 'C';
-    }
-    std::sort(guests.begin(), guests.end());
-
-    int last_c{-1}, last_a{-1};
-    std::vector<int> peer(G, -1), cpeer(G, -1);
-
-    for (int i = 0; i < N; ++i)
-        if (guests[i].second) {
-            peer[i] = last_c;
-            cpeer[i] = last_a;
-            last_c = i;
-        } else {
-            last_a = i;
-        }
-    if (~last_c) {
-        int i{0};
-        while (true) {
-            if (guests[i].second) {
-                peer[i] = last_c;
-                break;
-            }
-            cpeer[i] = last_c;
-            ++i;
-        }
-    }
-
-    for (int i = N - 1; ~i; --i)
-        if (!guests[i].second) {
-            peer[i] = last_a;
-            last_a = i;
-        } else
-            cpeer[i] = last_a;
-    if (~last_a) {
-        int i{N - 1};
-        while (true) {
-            if (!guests[i].second) {
-                peer[i] = last_a;
-                break;
-            }
-            cpeer[i] = last_a;
-            --i;
-        }
-    }
-
-    for (int i = 0; i < G; ++i) {
-        int peer_distance{0};
-        int peer_trace = std::min(M, N);
-        if (guests[i].second)
-            peer_distance = (N + i - peer[i]) % N;
+        if (g.second)
+            right[(g.first + M) % N] = M; // after M minutes clockwise guests reach here
         else
-            peer_distance = (N + peer[i] - i) % N;
-        if (peer_distance <= M)
-            peer_trace = peer_distance;
-
-        int final_pos = (guests[i].first + M) % N;
-        int last_collision = peer[cpeer[final_pos]];
-        int colide_distance{0};
-        if (guests[i].second)
-            colide_distance = (N + i - last_collision) % N;
-        else
-            colide_distance = (N + last_collision - i) % N;
-
-        std::cout << peer_trace - colide_distance << ' ';
+            left[(g.first - (M % N) + N) % N] = M;
     }
+
+    // compute directional time of the last visit for every consulate
+    int cur = -1;
+    for (int i = 0; i < 2 * N; ++i) {
+        cur = std::max(cur - 1, left[i % N]);
+        left[i % N] = cur;
+    }
+    cur = -1;
+    for (int i = 2 * N - 1; ~i; --i) {
+        cur = std::max(cur - 1, right[i % N]);
+        right[i % N] = cur;
+    }
+
+    // for every consulate score last visiting guest
+    std::vector<int> lcnt(N, 0), rcnt(N, 0);
+    for (int i = 0; i < N; ++i) {
+        const int dir = std::max(left[i], right[i]);
+        if (dir < 0) continue;
+        if (dir == left[i]) {
+            const int origin = (i + dir) % N;
+            ++lcnt[origin];
+        }
+        if (dir == right[i]) {
+            const int origin = (i - (dir % N) + N) % N;
+            ++rcnt[origin];
+        }
+    }
+
+    for (auto& [pos, dir] : guests)
+        std::cout << (dir ? rcnt : lcnt)[pos] << ' ';
 }
 
 int main(int, char**)
@@ -115,10 +65,7 @@ int main(int, char**)
     int no_of_cases;
     std::cin >> no_of_cases;
     for (int g = 1; g <= no_of_cases; ++g) {
-        // /*attach_trap();*/ solve();
         std::cout << "Case #" << g << ": "; solve(); std::cout << '\n';
-        //std::cout << "Case #" << g << ": " << (solve() ? "" : "IM") << "POSSIBLE\n";
-        //std::cout << "Case #" << g << ": " << std::fixed << std::setprecision(9); solve(); std::cout << '\n';
     }
 }
 
@@ -129,13 +76,31 @@ clang++.exe -Wall -Wextra -g -O0 -std=c++17 latest_guests.cpp -o latest_guests.e
 g++ -Wall -Wextra -g3 -Og -std=c++17 -fsanitize=address latest_guests.cpp -o latest_guests
 
 Run:
-py.exe interactive_runner.py py.exe latest_guests_testing_tool.py 1 -- latest_guests.exe
 latest_guests.exe < latest_guests.in
 
 Input:
 
+4
+5 3 2
+5 C
+2 A
+1 A
+2 4 0
+1 A
+1 C
+1 A
+1 C
+3 2 10
+3 C
+2 A
+6 1 6
+4 A
 
 Output:
 
+Case #1: 2 2 1 
+Case #2: 1 1 1 1
+Case #3: 2 2
+Case #4: 6
 
 */
