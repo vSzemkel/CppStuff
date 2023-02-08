5ba4cf77af28f7ac62772be3760c3307c11b06dc
Marcin Buchwald <marcin.buchwald@outlook.com>
2023-02-08 09:38:55 +0100

    [ADD] Teach Me task from Kickstart 2019 - Round F with custom hashing

--- kickstart/2019/teach_me.cpp
+++ kickstart/2019/teach_me.cpp
@@ -2,17 +2,61 @@
 #include <iostream>
 #include <map>
 #include <set>
+#include <unordered_map>
 #include <unordered_set>
 #include <vector>
 
 // Teach Me
 // https://codingcompetitions.withgoogle.com/kickstart/round/0000000000050edc/00000000001864bc
 
-static void solve() {
-    int64_t N, S;
+static int64_t hash(const std::set<int>& skills) {
+    constexpr const int64_t fac = 1003;
+    int64_t ans{};
+    for (const auto s : skills)
+        ans = ans * fac + s;
+    return ans;
+}
+
+// @Observation1: A can not teach B iff A skills are subset of B skills
+// @Observation2: Subset of up to 5 integers in range 1.1000 can be hashed as int64
+static void solve() { // O(N * 2^5 * 5) real 0m6.027s
+    int N, S;
     std::cin >> N >> S;
     std::vector<std::set<int>> people(N); // person -> set of skills
-    std::map<std::set<int>, int64_t> counts; // skill set -> number of mentors with this set
+    std::unordered_map<int64_t, int> counts; // skill set -> number of mentors with this set
+    for (auto& skills : people) {
+        int c; std::cin >> c;
+        for (int y = c; y; --y) {
+            int s; std::cin >> s;
+            skills.insert(s);
+        }
+        ++counts[hash(skills)];
+    }
+
+    auto ans = int64_t(N) * N; // tricky line, as N2 > INT_MAX
+    for (const auto& skills : people) {
+        const int ub = 1 << skills.size();
+        for (int mask = 0; mask < ub; ++mask) {
+            int check{1};
+            std::set<int> subset;
+            for (const auto s : skills) {
+                if (mask & check)
+                    subset.insert(s);
+                check <<= 1;
+            }
+            ans -= counts[hash(subset)];
+        }
+    }
+
+    std::cout << ans;
+}
+
+// @Observation1: A can not teach B iff A skills are subset of B skills
+static void solve_v1() { // O(N * 2^5 * 5) real 0m17.213s
+    int N, S;
+    std::cin >> N >> S;
+    std::vector<std::set<int>> people(N); // person -> set of skills
+    std::map<std::set<int>, int> counts; // skill set -> number of mentors with this set
     for (auto& skills : people) {
         int c; std::cin >> c;
         for (int y = c; y; --y) {
@@ -22,7 +66,7 @@ static void solve() {
         ++counts[skills];
     }
 
-    int64_t ans = N * N;
+    auto ans = int64_t(N) * N; // tricky line, as N2 > INT_MAX
     for (const auto& skills : people) {
         const int ub = 1 << skills.size();
         for (int mask = 0; mask < ub; ++mask) {
@@ -40,7 +84,7 @@ static void solve() {
     std::cout << ans;
 }
 
-static void solve_set1() {
+static void solve_set1() { // O(N2 * 5^2)
     int N, S;
     std::cin >> N >> S;
     std::vector<std::unordered_set<int>> skills(N); // skill -> number of teachers
