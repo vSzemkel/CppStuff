b3d0e7152376ded6af20cd2ad9cc033de7023c62
Marcin Buchwald <marcin.buchwald@outlook.com>
2023-03-05 10:00:57 +0100

    [FIX] Diagonal Puzzle task from Kickstart 2019 - Round G, still incomplete

--- kickstart/2019/diagonal_puzzle.cpp
+++ kickstart/2019/diagonal_puzzle.cpp
@@ -32,12 +32,14 @@
 // https://codingcompetitions.withgoogle.com/kickstart/round/0000000000050edd/00000000001a2835
 
 constexpr const int INF = 1e09;
-constexpr const int SLASHED = 1;
-constexpr const int BACK_SLASHED = 2;
+constexpr const int SPIN = 4;
+constexpr const int SLASHED = 2;
+constexpr const int BACK_SLASHED = 1;
 
 static void solve() {
     int N;
     std::cin >> N;
+    const int par = N & 1;
     const std::string pat(N, '#');
     std::vector<std::string> board(N);
     for (auto& r : board)
@@ -45,17 +47,20 @@ static void solve() {
 
     int ans{INF};
     const auto turn = [](char& c){ c = (c == '.' ? '#' : '.'); };
-    for (int c = 0; c < 4; ++c) {
+    for (int m = 0; m < (1 << (2 + par)); ++m) {
         int cur{0};
         auto copy = board;
-        if (c & SLASHED) {
+        if (m & SPIN)
+            for (int t = 0, b = N - 1; t < b; ++t, --b)
+                std::swap(copy[t], copy[b]);
+        if (m & SLASHED) {
             ++cur;
             for (int z = N, r = N - 1, c = 0; z; --z, --r, ++c)
                 turn(copy[r][c]);
         }
-        if (c & BACK_SLASHED) {
+        if (m & BACK_SLASHED) {
             ++cur;
-            for (int z = N, r = 0, c = 0; z; --z, ++r, ++c)
+            for (int z = N - par, r = 0, c = par; z; --z, ++r, ++c)
                 turn(copy[r][c]);
         }
 
@@ -72,20 +77,18 @@ static void solve() {
                     turn(copy[s][d]);
             }
 
-
-        for (int r = 0, c = 0; r < N / 2; ++r, ++c)
+        for (int r = 0, c = par; r < N / 2; ++r, ++c)
             if (copy[r][c] == '.') {
                 ++cur;
-                for (int y = 2 * r + 1, s = 2 * r, d = 0; y; --y, --s, ++d)
+                for (int y = 2 * r + 1 + par, s = 2 * r + par, d = 0; y; --y, --s, ++d)
                     turn(copy[s][d]);
             }
-        for (int r = N - 1, c = N - 1; r >= N / 2; --r, --c)
+        for (int r = N - 1 - par, c = N - 1; r >= N / 2; --r, --c)
             if (copy[r][c] == '.') {
                 ++cur;
-                for (int y = 2 * (N - r) - 1, s = N - 1, d = N - y; y; --y, --s, ++d)
+                for (int y = N - r, s = N - 1, d = r; y; --y, --s, ++d)
                     turn(copy[s][d]);
             }
-
 
         if (std::all_of(copy.begin(), copy.end(), [&pat](auto& r){ return r == pat; }))
             ans = std::min(ans, cur);
