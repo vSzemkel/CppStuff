5f714d170b54a4c5958e7cdb4195bd56ba650a54
Marcin Buchwald <marcin.buchwald@outlook.com>
2023-04-23 10:42:31 +0200

    [ADD] Codeforces 1810, Tasks E - Monsters, incomplete

---
+++ codeforces/1810/monsters.cpp
@@ -0,0 +1,127 @@
+
+#include <iostream>
+#include <queue>
+#include <utility>
+#include <vector>
+
+// Monsters
+// https://codeforces.com/contest/1810/problem/E
+
+static void solve() {
+    int V, E;
+    std::cin >> V >> E;
+    std::queue<int> q;
+    std::vector<int> dangers(V);
+    for (auto& d : dangers)
+        std::cin >> d;
+    std::vector<std::vector<int>> graph(V);
+    for (int z = E; z; --z) {
+        int f, t;
+        std::cin >> f >> t;
+        --f, --t;
+        graph[f].push_back(t);
+        graph[t].push_back(f);
+    }
+
+    for (int start = 0; start < V; ++start)
+        if (dangers[start] == 0) {
+            std::priority_queue<std::pair<int, int>, std::vector<std::pair<int, int>>, std::greater<>> reachable_alive; // {denger, index}
+            std::vector<bool> defeated(V), inqueue(V);
+            int dead{0};
+            q.push(start);
+            inqueue[start] = true;
+            while (!q.empty()) {
+                const int cur = q.front();
+                q.pop();
+                defeated[cur] = true;
+                ++dead;
+                for (const int next : graph[cur])
+                    if (!defeated[next] && !inqueue[next]) {
+                        if (dangers[next] <= dead) {
+                            q.push(next);
+                            inqueue[next] = true;
+                        } else
+                            reachable_alive.emplace(dangers[next], next);
+                    }
+
+                while (!reachable_alive.empty() && reachable_alive.top().first <= dead) {
+                    const int can = reachable_alive.top().second;
+                    if (!inqueue[can])
+                        q.push(can);
+                    reachable_alive.pop();
+                }
+            }
+
+            if (dead == V) {
+                std::cout << "YES";
+                return;
+            }
+        }
+
+    std::cout << "NO";
+}
+
+int main(int, char**)
+{
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+
+    int no_of_cases;
+    std::cin >> no_of_cases;
+    for (int g = 1; g <= no_of_cases; ++g) {
+        solve(); std::cout << '\n';
+    }
+}
+
+/*
+
+Compile:
+cls && clang++.exe -Wall -Wextra -g -O0 -std=c++17 monsters.cpp -o monsters.exe
+g++ -Wall -Wextra -g3 -Og -std=c++17 -fsanitize=address monsters.cpp -o monsters
+
+Run:
+py.exe interactive_runner.py py.exe monsters_testing_tool.py 1 -- monsters.exe
+monsters.exe < monsters.in
+
+Input:
+
+5
+4 3
+2 1 0 3
+1 2
+2 3
+3 4
+6 6
+0 1 2 3 0 1
+1 2
+2 3
+3 4
+4 5
+4 6
+5 6
+4 3
+0 1 2 0
+1 2
+2 3
+1 3
+4 6
+1 1 1 0
+1 2
+3 2
+4 3
+2 4
+4 1
+1 3
+5 5
+0 1 3 2 0
+1 2
+2 3
+3 4
+4 5
+3 5
+
+Output:
+
+
+*/
