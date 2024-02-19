718249699510d67a204791886a84ab9f113b850f
Marcin Buchwald <marcin.buchwald@outlook.com>
2024-02-19 14:46:39 +0100

    [BUG] Fix bug in activity selection task solution

--- classic/activity_selection.cpp
+++ classic/activity_selection.cpp
@@ -3,19 +3,19 @@
 #include <array>
 #include <format>
 #include <iostream>
+#include <vector>
 
 // Activity selection
 // Given a set of activities with [start, finish) times schedule 
 // maximal number non overlapping activities
 
-constexpr const int N = 13;
+constexpr const int N = 12;
 
 std::array<std::array<int, 2>, N> activities = {
-    std::array<int, 2>{0, 4},
+    std::array<int, 2>{1, 4},
     {3, 6},
     {5, 6},
     {0, 10},
-    {0, 5},
     {6, 8},
     {4, 5},
     {8, 10},
@@ -23,7 +23,7 @@ std::array<std::array<int, 2>, N> activities = {
     {4, 8},
     {1, 3},
     {6, 7},
-    {9, 10},
+    {9, 10}
 };
 
 /**
@@ -32,15 +32,22 @@ std::array<std::array<int, 2>, N> activities = {
  */
 int main(int, char**)
 {
-    std::sort(activities.begin(), activities.end());
-    int next_start{}, ord{0};
-    for (const auto& [start, stop] : activities)
-        if (next_start <= start) {
-            std::cout << std::format("{}. [{}, {})\n", ++ord, start, stop);
-            next_start = stop;
-        }
+    std::ranges::sort(activities);
 
-    std::cout << "Maximum " << ord << " activities can be arranged\n";
+    std::vector<std::array<int, 2>> stack(1, {-1, -1});
+    for (const auto& [start, stop] : activities) {
+        if (stop < stack.back()[1])
+            stack.pop_back();
+        if (stack.back()[1] <= start)
+            stack.push_back({start, stop});
+    }
+
+    stack.erase(stack.begin());
+    std::cout << std::format("Maximum {} activities can be arranged\n", stack.size());
+
+    int ord{0};
+    for (const auto& [start, stop] : stack)
+        std::cout << std::format("{}. [{}, {})\n", ++ord, start, stop);
 }
 
 /*
@@ -51,11 +58,11 @@ g++ -Wall -Wextra -ggdb3 -Og -std=c++20 -fsanitize=address activity_selection.cp
 
 Output:
 
-Maximum 4 activities can be arranged
+Maximum 5 activities can be arranged
 1. [1, 3)
 2. [4, 5)
 3. [5, 6)
-4. [6, 8)
-5. [9, 10)
+4. [6, 7)
+5. [8, 10)
 
 */
