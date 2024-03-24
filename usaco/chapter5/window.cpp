dfa9f5db1dd08283cdd7d8fa44c911b37342dbf2
VELVET-PC\velvet <marcin.buchwald@outlook.com>
2024-03-24 14:04:18 +0100

    [ADD] Window problem from USACO, incomplete

--- usaco/chapter5/window.cpp
+++ usaco/chapter5/window.cpp
@@ -10,6 +10,7 @@ PROBLEM STATEMENT:
 #include <cmath>
 #include <cstdio>
 #include <fstream>
+#include <iomanip>
 #include <string>
 #include <vector>
 
@@ -20,9 +21,30 @@ struct window_t
 {
     char id;
     int x, y, X, Y;
+    float cache;
+    int area() const {
+        return (X - x) * (Y - y);
+    }
 };
 
 std::vector<window_t> screen;
+
+void print(const std::vector<window_t>::iterator mid)
+{
+    //if (mid->cache < 0) {
+        int covered{}, total = mid->area();
+        for (int i = mid->x; i < mid->X; ++i)
+            for (int j = mid->y; j < mid->Y; ++j)
+                if (std::any_of(mid + 1, screen.end(), [i, j](const auto& f){
+                    return f.x <= i && i < f.X && f.y <= j && j < f.Y;
+                }))
+                    ++covered;
+
+        mid->cache = 100.0 * (total - covered) / total;
+    //}
+
+    task_out << std::fixed << std::setprecision(3) << mid->cache << '\n';
+}
 
 int main(int, char**)
 {
@@ -30,6 +52,7 @@ int main(int, char**)
     while (std::getline(task_in, line)) {
         char action, id;
         int x, y, X, Y;
+        //assert(2 == sscanf(line.data(), "%c(%c", &action, &id));
         assert(2 == sscanf_s(line.data(), "%c(%c", &action, 1, &id, 1));
         const auto mid = std::find_if(screen.begin(), screen.end(), [id](const auto& w){ return w.id == id; });
         assert(action == 'w' || mid != screen.end());
@@ -37,25 +60,30 @@ int main(int, char**)
         switch (action)
         {
         case 'w':
+            //assert(4 == sscanf(line.data() + 4, "%d,%d,%d,%d)", &x, &y, &X, &Y));
             assert(4 == sscanf_s(line.data() + 4, "%d,%d,%d,%d)", &x, &y, &X, &Y));
-            screen.push_back(window_t{id, std::min(x, X), std::min(y, Y), std::max(x, X), std::max(y, Y)});
+            screen.push_back(window_t{id, std::min(x, X), std::min(y, Y), std::max(x, X), std::max(y, Y), -1.});
+            std::for_each(screen.begin(), screen.end(), [](auto& w){ w.cache = -1.; });
             break;
         case 't':
-            assert(mid != screen.end());
             std::rotate(mid, mid + 1, screen.end());
+            std::for_each(mid, screen.end(), [](auto& w){ w.cache = -1.; });
             break;
         case 'b':
-            assert(mid != screen.end());
             std::rotate(screen.begin(), mid, mid + 1);
+            std::for_each(screen.begin(), mid, [](auto& w){ w.cache = -1.; });
             break;
         case 'd':
             screen.erase(mid);
             break;
-        default:
+        case 's': {
+            print(mid);
             break;
         }
+        default:
+            assert(true);
+        }
     }
-    task_out << '\n';
 }
 
 /*
@@ -66,8 +94,28 @@ g++ -Wall -Wextra -ggdb3 -Og -std=c++17 -fsanitize=address window.cpp -o window
 
 Input:
 
+w(s,26368,31524,7661,22131) 
+s(s) 
+w(E,23902,16575,17367,18133) 
+w(0,2548,19609,32409,3058) 
+s(E) 
+s(s) 
+s(s) 
+w(Z,16613,8720,8446,2823) 
+s(Z) 
+b(Z) 
+s(Z) 
+t(0) 
+s(E)
 
 Output:
 
+100.000
+0.000
+100.000
+100.000
+100.000
+3.985
+0.000
 
 */
