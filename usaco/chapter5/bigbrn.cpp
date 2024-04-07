ef060626471b47f201b7f0c53cb7cc344eda29ff
Marcin Buchwald <marcin.buchwald@outlook.com>
2024-04-07 12:45:18 +0200

    [ADD] Big barn task from USACO, 11/15

--- usaco/chapter5/bigbrn.cpp
+++ usaco/chapter5/bigbrn.cpp
@@ -5,6 +5,7 @@ TASK: bigbrn
 PROBLEM STATEMENT: https://usaco.training/usacoprob2?a=FWfFlfCOnfs&S=bigbrn
 */
 
+#include <algorithm>
 #include <fstream>
 #include <vector>
 
@@ -13,30 +14,43 @@ std::ofstream task_out("bigbrn.out");
 
 int main(int, char**)
 {
-    int N, T;
+    int N, T, r, c;
     std::string comet, group;
     task_in >> N >> T;
+    int bbc{N}, bbC{}, bbr{N}, bbR{};
     std::vector<std::vector<bool>> trees(N, std::vector<bool>(N));
-    for (; T; --T) {
-        int r, c;
+    for (int z = T; z; --z) {
         task_in >> r >> c;
         trees[--r][--c] = true;
+        bbr = std::min(bbr, r);
+        bbR = std::max(bbR, r);
+        bbc = std::min(bbc, c);
+        bbC = std::max(bbC, c);
     }
 
-    int ans;
+    bool found{};
     std::vector<std::vector<bool>> barns(N, std::vector<bool>(N, true));
-    for (ans = 0; ans < N; ++ans) {
-        bool found{};
-        for (int r = 0; r < N - ans; ++r)
-            for (int c = 0; c < N - ans; ++c)
-                if (barns[r][c] && barns[r + ans][c + ans] && !trees[r + ans][c] && !trees[r][c + ans])
-                    found = true;
-                else
-                    barns[r][c] = false;
+    for (int r = 0; r < N; ++r)
+        for (int c = 0; c < N; ++c) {
+            barns[r][c] = !trees[r][c];
+            if (barns[r][c])
+                found = true;
+        }
 
-        if (!found)
-            break;
-    }
+    int ans = std::max({bbc, bbr, N - 1 - bbC, N - 1 - bbR});
+    if (found && T > 1)
+        for (ans = 1; ans < N; ++ans) {
+            found = false;
+            for (int r = 0; r < N - ans; ++r)
+                for (int c = 0; c < N - ans; ++c)
+                    if (barns[r][c] && barns[r + 1][c + 1] && !trees[r + ans][c] && !trees[r][c + ans])
+                        found = true;
+                    else
+                        barns[r][c] = false;
+
+            if (!found)
+                break;
+        }
 
     task_out << ans << '\n';
 }
