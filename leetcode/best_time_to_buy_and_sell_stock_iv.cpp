2bbdca89ed33abf549e30814f75026c95c122ed0
Marcin Buchwald <marcin.buchwald@outlook.com>
2024-05-19 12:21:09 +0200

    [ADD] Best Time to Buy and Sell Stock IV task from Leetcode, slow

--- leetcode/best_time_to_buy_and_sell_stock_iv.cpp
+++ leetcode/best_time_to_buy_and_sell_stock_iv.cpp
@@ -10,24 +10,12 @@
 std::unordered_map<int, int> memo; // hash -> profit
 std::vector<int> prices;
 
-/**
- * @brief Unique hash of given parameters can be stored in 32 bit
- * 
- * @param k range 1..100
- * @param begin range 1..1000
- * @param end range 1..1000
- * @return int hash
- */
-int hash(const int k, const int begin, const int end) {
-    return (begin * 1001 + end) * 1001 + k;
-}
-
 int maxProfitInternal(const int k, const int begin, const int end) {
     const auto sz = end - begin;
     if (sz < 2)
         return 0;
-    const auto h = hash(k, begin, end);
-    const auto found = memo.find(h);
+    const auto hash = (((begin << 10) + end) << 10) + k;
+    const auto found = memo.find(hash);
     if (found != memo.end())
         return found->second;
 
@@ -42,18 +30,20 @@ int maxProfitInternal(const int k, const int begin, const int end) {
     int max = prices.back();
     if (k > 1)
         for (int i = end - 2, b = i - begin; i > begin + 1; --i, --b) { // left of size >= 2 to be effective
-            if (k > 2)
-                ans = std::max(ans, best_left[b] + maxProfitInternal(k - 1, i, end));
-            else
+            if (k > 2) {
+                const auto nk = std::min(k - 1, (end - i) / 2);
+                ans = std::max(ans, best_left[b] + maxProfitInternal(nk, i, end));
+            } else
                 ans = std::max(ans, best_left[b] + max - prices[i]);
             max = std::max(max, prices[i]);
         }
 
-    return memo[h] = ans;
+    return memo[hash] = ans;
 }
 
 int maxProfit(int k, std::vector<int>& _prices) {
     prices = _prices;
+    memo.reserve(k * k * k);
     return maxProfitInternal(k, 0, int(prices.size()));
 }
 
