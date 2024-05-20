c354f71be0ee183475c89b1e611a185c21d3488d
marcin.buchwald@outlook.com <marcin.buchwald@outlook.com>
2024-05-20 13:30:49 +0200

    [ADD] Best Time to Buy and Sell Stock IV task from Leetcode

--- leetcode/best_time_to_buy_and_sell_stock_iv.cpp
+++ leetcode/best_time_to_buy_and_sell_stock_iv.cpp
@@ -1,16 +1,48 @@
 
 #include <algorithm>
+#include <array>
 #include <iostream>
+#include <ranges>
 #include <unordered_map>
 #include <vector>
 
 // Best Time to Buy and Sell Stock with at most K transactions
 // https://leetcode.com/problems/best-time-to-buy-and-sell-stock-iv
 
+/**
+ * We will analyze sequence of prices from the end.
+ * Every day we can or can not buy a stock
+ * The decision is evaluated in context of possible future continuation
+*/
+int maxProfit(int k, std::vector<int>& prices) {
+    using dp_t = std::vector<std::array<int, 2>>;
+    dp_t current(k + 1), future(k + 1);
+
+    for (const int price : prices | std::views::reverse) {
+        for (int limit = 1; limit <= k; ++limit)
+            for (int can_buy = 0; can_buy < 2; ++can_buy) {
+                auto& profit = current[limit][can_buy];
+                // max(act, wait)
+                if (can_buy)
+                    profit = std::max(-price + future[limit][0], future[limit][1]);
+                else
+                    profit = std::max(price + future[limit - 1][1], future[limit][0]);
+            }
+
+        std::swap(future, current);
+    }
+
+    return future[k][1];
+}
+
+/**
+ * Slow implementation is direct generalization of the approach taken in best-time-to-buy-and-sell-stock-iii
+ * It is not a crap but there is much better algorithm
+*/
 std::unordered_map<int, int> memo; // hash -> profit
 std::vector<int> prices;
 
-int maxProfitInternal(const int k, const int begin, const int end) {
+int maxProfitSlowInternal(const int k, const int begin, const int end) {
     const auto sz = end - begin;
     if (sz < 2)
         return 0;
@@ -32,7 +64,7 @@ int maxProfitInternal(const int k, const int begin, const int end) {
         for (int i = end - 2, b = i - begin; i > begin + 1; --i, --b) { // left of size >= 2 to be effective
             if (k > 2) {
                 const auto nk = std::min(k - 1, (end - i) / 2);
-                ans = std::max(ans, best_left[b] + maxProfitInternal(nk, i, end));
+                ans = std::max(ans, best_left[b] + maxProfitSlowInternal(nk, i, end));
             } else
                 ans = std::max(ans, best_left[b] + max - prices[i]);
             max = std::max(max, prices[i]);
@@ -41,10 +73,10 @@ int maxProfitInternal(const int k, const int begin, const int end) {
     return memo[hash] = ans;
 }
 
-int maxProfit(int k, std::vector<int>& _prices) {
+int maxProfitSlow(int k, std::vector<int>& _prices) {
     prices = _prices;
     memo.reserve(k * k * k);
-    return maxProfitInternal(k, 0, int(prices.size()));
+    return maxProfitSlowInternal(k, 0, int(prices.size()));
 }
 
 static void io_handler()
