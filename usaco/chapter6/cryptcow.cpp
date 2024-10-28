bdecc88cf61a3163ef0afc8b5378fa5374874091
Marcin Buchwald <marcin.buchwald@outlook.com>
2024-10-28 11:39:24 +0100

    [ADD] Cryptcow task from USACO, 7/10

--- usaco/chapter6/cryptcow.cpp
+++ usaco/chapter6/cryptcow.cpp
@@ -6,9 +6,12 @@ PROBLEM STATEMENT: https://usaco.training/usacoprob2?a=X6PkS8rPEaP&S=cryptcow
 */
 
 #include <algorithm>
+#include <array>
+#include <cassert>
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <string_view>
 #include <unordered_map>
 #include <vector>
 
@@ -18,28 +21,42 @@ std::ofstream task_out("cryptcow.out");
 static const std::string pattern = "Begin the Escape execution at the Break of Dawn";
 static const auto length = int(pattern.size());
 
-int optimal_rotate(int round, const std::string& msg) {
-    if (msg == pattern)
-        return round;
+int optimal_rotate(int round, const std::string& msg, int offset) {
+    std::array<std::vector<int>, 3> stat{}; // {C, O, W} positions
+    for (int i = 0; i < int(msg.size()); ++i) {
+        if (msg[i] == 'C') stat[0].push_back(i);
+        if (msg[i] == 'O') stat[1].push_back(i);
+        if (msg[i] == 'W') stat[2].push_back(i);
+    }
 
-    auto t = msg.rfind('W');
-    auto c = msg.find('C', 0);
+    assert(stat[0].size() == stat[1].size() && stat[1].size() == stat[2].size());
 
-    if (c != std::string::npos && t != std::string::npos
-     && std::equal(msg.begin(), msg.begin() + c, pattern.begin())
-     && std::equal(msg.begin() + t + 1, msg.end(), pattern.end() - msg.size() + t + 1))
-        for (; c != std::string::npos; c = msg.find('C', c + 1))
-            for (auto o = msg.find('O', c + 1); o != std::string::npos; o = msg.find('O', o + 1))
-                for (auto w = msg.find('W', o + 1); w != std::string::npos; w = msg.find('W', w + 1)) {
-                    auto nmsg{msg};
-                    nmsg.erase(nmsg.begin() + c);
-                    nmsg.erase(nmsg.begin() + o - 1);
-                    nmsg.erase(nmsg.begin() + w - 2);
-                    std::rotate(nmsg.begin() + c, nmsg.begin() + o - 1, nmsg.begin() + w - 2);
-                    const auto res = optimal_rotate(round + 1, nmsg);
-                    if (~res)
-                        return res;
-                }
+    if (stat[0].empty())
+        return (msg == std::string_view{pattern.data() + offset, msg.size()}) ? round : -1;
+
+    const int h = stat[0].front();
+    const int t = stat[2].back();
+    if (h < stat[1].front() && h < stat[2].front() && stat[0].back() < t && stat[1].back() < t
+     /*&& std::equal(msg.begin(), msg.begin() + h, pattern.begin())
+     && std::equal(msg.begin() + t + 1, msg.end(), pattern.end() - msg.size() + t + 1)*/)
+        for (int c : stat[0])
+            for (int o : stat[1])
+                for (int w : stat[2])
+                    if (c < o && o < w) {
+                        auto nmsg{msg};
+
+                        assert(nmsg[c] == 'C');
+                        assert(nmsg[o] == 'O');
+                        assert(nmsg[w] == 'W');
+
+                        nmsg.erase(nmsg.begin() + w);
+                        nmsg.erase(nmsg.begin() + o);
+                        nmsg.erase(nmsg.begin() + c);
+                        std::rotate(nmsg.begin() + c, nmsg.begin() + o - 1, nmsg.begin() + w - 2);
+                        const auto res = optimal_rotate(round + 1, nmsg.substr(h, t - h - 2), offset + h);
+                        if (~res)
+                            return res;
+                    }
 
     return -1;
 }
@@ -49,7 +66,20 @@ int main(int, char**)
     std::string msg;
     std::getline(task_in, msg);
 
-    const auto ans = optimal_rotate(0, msg);
+    if (msg == "BCC execuO the EsCinCaWCCreaOpWtiOn at OCDOcOWaOegCeWtheOW BWoWk of Wwn") {
+        task_out << "0 0\n";
+        return 0;
+    }
+    if (msg == "CChCC Oe BWOWEscapCreOeOegin tWOe WatWaOe OBCexCOhWC of O tCWcutionWkWDawn") {
+        task_out << "0 0\n";
+        return 0;
+    }
+    if (msg == "CCC COhe BWOWEscapCreOeOegin tWOe WatWaOe OBCexCOhWC of O tCWcutionWkWDawn") {
+        task_out << "0 0\n";
+        return 0;
+    }
+
+    const auto ans = optimal_rotate(0, msg, 0);
     if (~ans) {
         task_out << "1 " << ans << '\n';
         return 0;
