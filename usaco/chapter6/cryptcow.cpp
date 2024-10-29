ab1594d777b09269258907a962e755d0479a8b7d
Marcin Buchwald <marcin.buchwald@outlook.com>
2024-10-29 11:15:28 +0100

    [ADD] Cryptcow task from USACO, 7/10 shortest

--- usaco/chapter6/cryptcow.cpp
+++ usaco/chapter6/cryptcow.cpp
@@ -6,52 +6,83 @@ PROBLEM STATEMENT: https://usaco.training/usacoprob2?a=X6PkS8rPEaP&S=cryptcow
 */
 
 #include <algorithm>
-#include <array>
-#include <cassert>
 #include <iostream>
 #include <fstream>
 #include <string>
 #include <string_view>
+#include <unordered_set>
 #include <vector>
 
 std::ifstream task_in("cryptcow.in");
 std::ofstream task_out("cryptcow.out");
 
+static const auto is_cow = [](const char c){ return c == 'C' || c == 'O' || c == 'W'; };
 static const std::string pattern = "Begin the Escape execution at the Break of Dawn";
 static const auto length = int(pattern.size());
+static const std::hash<std::string_view> hh{};
+static std::unordered_set<size_t> seen;
 
-int optimal_rotate(int round, const std::string& msg, int offset) {
-    std::array<std::vector<int>, 3> stat{}; // {C, O, W} positions
-    for (int i = 0; i < int(msg.size()); ++i) {
-        if (msg[i] == 'C') stat[0].push_back(i);
-        if (msg[i] == 'O') stat[1].push_back(i);
-        if (msg[i] == 'W') stat[2].push_back(i);
+bool check_middle(std::string_view msg)
+{
+    auto stop = msg.begin();
+    while (true) {
+        const auto start = std::find_if(stop, msg.end(), [](const char c){ return !is_cow(c); });
+        stop = std::find_if(start, msg.end(), [](const char c){ return is_cow(c); });
+        if (start == msg.end())
+            return true;
+        if (pattern.find(std::string_view{&*start, static_cast<size_t>(stop - start)}) == std::string::npos)
+            return false;
+    }
+}
+
+int optimal_rotate(int round, std::string_view msg, int offset)
+{
+    if (msg.size() < 54) {
+        auto hash = hh(msg);
+        // hash ^= (offset * 51);
+        if (seen.count(hash) > 0)
+            return -1;
+        seen.insert(hash);
     }
 
-    if (stat[0].empty())
+    std::vector<int> stat_c, stat_o, stat_w;
+    for (int i = 0; i < int(msg.size()); ++i) {
+        if (msg[i] == 'C') stat_c.push_back(i);
+        if (msg[i] == 'O') stat_o.push_back(i);
+        if (msg[i] == 'W') stat_w.push_back(i);
+    }
+
+    if (stat_c.empty())
         return (msg == std::string_view{pattern.data() + offset, msg.size()}) ? round : -1;
 
-    const int h = stat[0].front();
-    const int t = stat[2].back();
+    const int h = stat_c.front();
+    const int t = stat_w.back();
+    const size_t nlen = t - h - 2;
 
-    if (h < stat[1].front() && h < stat[2].front() && stat[0].back() < t && stat[1].back() < t
-    // && std::equal(msg.begin(), msg.begin() + h, pattern.begin() + offset)
-    // && std::equal(msg.begin() + t + 1, msg.end(), pattern.begin() + offset + t + 1 - (3 * stat[0].size()))
-    )
-        for (int c : stat[0])
-            for (int o : stat[1])
+    if (h < stat_o.front() && h < stat_w.front() && stat_c.back() < t && stat_o.back() < t
+       && std::equal(msg.begin(), msg.begin() + h, pattern.begin() + offset)
+       && std::equal(msg.begin() + t + 1, msg.end(), pattern.begin() + offset + t + 1 - (3 * stat_c.size()))
+       && check_middle(std::string_view{msg.data() + h, nlen})
+    ) {
+        const auto noff = offset + h;
+
+        for (int c : stat_c)
+            for (int o : stat_o)
                 if (c < o)
-                    for (int w : stat[2])
+                    for (int w : stat_w)
                         if (o < w) {
-                            auto nmsg{msg};
-                            nmsg.erase(nmsg.begin() + w);
-                            nmsg.erase(nmsg.begin() + o);
-                            nmsg.erase(nmsg.begin() + c);
-                            std::rotate(nmsg.begin() + c, nmsg.begin() + o - 1, nmsg.begin() + w - 2);
-                            const auto res = optimal_rotate(round + 1, nmsg.substr(h, t - h - 2), offset + h);
+                            std::string nmsg;
+                            nmsg.resize(nlen);
+                            auto to_write = nmsg.begin();
+                            to_write = std::copy_n(msg.begin() + h, c - h, to_write);
+                            to_write = std::copy_n(msg.begin() + o + 1, w - o - 1, to_write);
+                            to_write = std::copy_n(msg.begin() + c + 1, o - c - 1, to_write);
+                                       std::copy_n(msg.begin() + w + 1, t - w, to_write);
+                            const auto res = optimal_rotate(round + 1, nmsg, noff);
                             if (~res)
                                 return res;
                         }
+    }
 
     return -1;
 }
@@ -61,8 +92,8 @@ int main(int, char**)
     std::string msg;
     std::getline(task_in, msg);
 
-    if (msg == "BCC execuO the EsCinCaWCCreaOpWtiOn at OCDOcOWaOegCeWtheOW BWoWk of Wwn") {
-        task_out << "0 0\n";
+    /*if (msg == "BeCOgC CC execuOf DOBCiCCrWaOOt theCOCeak oWWin Oon aWtheOOW EscapeWtWWWwn") {
+        task_out << "1 9\n";
         return 0;
     }
     if (msg == "CChCC Oe BWOWEscapCreOeOegin tWOe WatWaOe OBCexCOhWC of O tCWcutionWkWDawn") {
@@ -72,7 +103,7 @@ int main(int, char**)
     if (msg == "CCC COhe BWOWEscapCreOeOegin tWOe WatWaOe OBCexCOhWC of O tCWcutionWkWDawn") {
         task_out << "0 0\n";
         return 0;
-    }
+    }*/
 
     const auto ans = optimal_rotate(0, msg, 0);
     if (~ans) {
