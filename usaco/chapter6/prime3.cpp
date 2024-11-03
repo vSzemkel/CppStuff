6e846f2aa1098e96d731775ee1f2781fccc660ea
Marcin Buchwald <marcin.buchwald@outlook.com>
2024-11-03 08:58:28 +0100

    [ADD] Prime3 task from USACO, less incomplete

--- usaco/chapter6/prime3.cpp
+++ usaco/chapter6/prime3.cpp
@@ -38,7 +38,7 @@ std::ofstream task_out("prime3.out");
 
 using board_t = std::array<std::string, 5>;
 
-static std::array<std::vector<std::pair<int, std::string>>, 10> generate(const int64_t lower, const int64_t upper, const int sum) { // generate primes in [range)
+static std::pair<std::array<std::vector<int>, 10>, std::array<std::vector<std::string>, 10>> generate(const int64_t lower, const int64_t upper, const int sum) { // generate primes in [range)
     std::vector<bool> taken(upper);
     const auto sqrt = (int)std::sqrt(upper);
     for (int i = 2; i <= sqrt; ++i)
@@ -46,7 +46,8 @@ static std::array<std::vector<std::pair<int, std::string>>, 10> generate(const i
             for (int j = i * i; j < upper; j += i)
                 taken[j] = true;
 
-    std::array<std::vector<std::pair<int, std::string>>, 10>primes;
+    std::array<std::vector<int>, 10> iprimes;
+    std::array<std::vector<std::string>, 10> sprimes;
     for (int i = 2; i < upper; ++i)
         if (!taken[i] && lower <= i) {
             int s{}, c{i};
@@ -54,11 +55,13 @@ static std::array<std::vector<std::pair<int, std::string>>, 10> generate(const i
                 s += (c % 10);
                 c /= 10;
             }
-            if (c + s == sum)
-                primes[c].emplace_back(i, std::to_string(i));
+            if (c + s == sum) {
+                iprimes[c].push_back(i);
+                sprimes[c].push_back(std::to_string(i));
+            }
         }
 
-    return primes;
+    return {iprimes, sprimes};
 }
 
 /**
@@ -69,14 +72,49 @@ int main(int, char**)
     int S, N;
     task_in >> S >> N;
 
-    const auto primes = generate(10000, 100000, S);
-    for (const auto& top : primes[N])
-        if (top.find('0') == std::string::npos)
-            for (const auto& r1 : primes[N])
-                for (int c = 0; c < 5; ++c) {
-                }
+    const auto [iprimes, sprimes] = generate(10000, 100000, S);
+    const auto check = [&](const std::string& s) {
+        const auto& bucket = sprimes[s.front() - '0'];
+        return std::binary_search(bucket.begin(), bucket.end(), s);
+    };
 
-    task_out << 0 << '\n';
+    bool found{};
+    for (const auto& top : sprimes[N])
+        if (top.find('0') == std::string::npos)
+            for (const auto& r1 : sprimes[N]) {
+                for (const auto& left : sprimes[N]) {
+                    if (left.find('0') == std::string::npos)
+                        continue;
+                    for (int c = 1; c < 5; ++c) {
+                        // TODO
+                    }
+                    if (check(board[2]) && check(board[3]) && check(board[4])) {
+                        std::string diag.
+                        diag.resize(5);
+                        diag[0] = N;
+                        diag[1] = r1[1];
+                        diag[2] = board[2][2];
+                        diag[3] = board[3][3];
+                        diag[4] = board[4][4];
+                        if (check(diag)) {
+                            diag[0] = board[4][0];
+                            diag[1] = board[3][1];
+                            diag[3] = r1[3];
+                            diag[4] = top.back();
+                            if (check(diag)) {
+                                found = true;
+                                std::cout << top << '\n' << r1 << '\n';
+                                for (r = 2; r < 5; ++r)
+                                    std::cout << board[r] << '\n';
+                                std::cout << '\n';
+                            }
+                        }
+                    }
+                }
+            }
+
+    if (!found)
+        task_out << "NONE\n";
 }
 
 /*
