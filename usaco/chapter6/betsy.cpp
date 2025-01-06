a3775dd99efe6f6c380b2c50401874cd22b0acf4
Marcin Buchwald <marcin.buchwald@outlook.com>
2025-01-06 11:49:04 +0100

    [OPT] Betsy task from USACO, 6/7

--- usaco/chapter6/betsy.cpp
+++ usaco/chapter6/betsy.cpp
@@ -5,6 +5,7 @@ TASK: betsy
 PROBLEM STATEMENT: https://usaco.training/usacoprob2?a=kqiaO7Hw9Oq&S=betsy
 */
 
+#include <bit>
 #include <fstream>
 #include <unordered_map>
 #include <utility>
@@ -13,7 +14,7 @@ PROBLEM STATEMENT: https://usaco.training/usacoprob2?a=kqiaO7Hw9Oq&S=betsy
 std::ifstream task_in("betsy.in");
 std::ofstream task_out("betsy.out");
 
-int N, N2, STOP, MEMO_LIMIT{40};
+int N, N2, STOP, HALFWAY, MIDPOINT, MEMO_LIMIT{40};
 uint64_t FULL, ONE{1};
 
 std::vector<std::unordered_map<uint64_t, int>> memo; // [pos][already visited] = count
@@ -23,6 +24,9 @@ int betsy(const int pos, const uint64_t visited)
     if (visited == FULL)
         return 1;
 
+    if (pos == MIDPOINT && (N & 1) && std::popcount(visited) < HALFWAY)
+        return 0;
+
     if (pos > MEMO_LIMIT && memo[pos - MEMO_LIMIT].count(visited))
         return memo[pos - MEMO_LIMIT][visited];
 
@@ -31,7 +35,7 @@ int betsy(const int pos, const uint64_t visited)
         const auto can = pos + move;
         const auto bit = ONE << can;
         const auto nvis = visited | bit;
-        if (!(visited & bit) && (can != STOP || nvis == FULL)) {
+        if (visited ^ nvis) {
             const auto col = pos % N;
             if (0 <= can && can < N2 && (0 < col || move != -1) && (col < N - 1 || move != 1)) {
                 if (can < N && move == -N && !(visited & (ONE << (can - 1))))
@@ -39,9 +43,10 @@ int betsy(const int pos, const uint64_t visited)
                 if (STOP < can && can < N2 - 1 && move == N && !(visited & (ONE << (can + 1))))
                     continue;
                 if (can > N && !(visited & (ONE << (can - N)))) {
-                    if ((can % N) == 0 && move == -1)
+                    const auto ncol = can % N;
+                    if (ncol == 0 && move == -1)
                         continue;
-                    if ((can % N) == N - 1 && move == 1)
+                    if (ncol == N - 1 && move == 1)
                         continue;
                 }
                 ans += betsy(can, nvis);
@@ -60,10 +65,15 @@ int main(int, char**)
     task_in >> N;
     N2 = N * N;
     STOP = N2 - N;
+    HALFWAY = N2 / 2 + 2;
+    MIDPOINT = N * (N / 2);
     if (N2 > MEMO_LIMIT)
         memo.resize(N2 - MEMO_LIMIT);
-    FULL = (ONE << N2) - 1;
-    task_out << betsy(0, 1) << '\n';
+    FULL = ((ONE << N2) - 1);
+    auto score = betsy(0, (ONE << STOP) + 1);
+    if (N & 1)
+        score *= 2;
+    task_out << std::max(1,score) << '\n';
 }
 
 /*
