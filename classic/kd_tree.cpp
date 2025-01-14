74f58ff0afa85d8bb7784c3de0c3d1024530d91c
Marcin Buchwald <marcin.buchwald@outlook.com>
2025-01-14 13:43:51 +0100

    [ADD] Alternative implementation of kd_tree not using any pointers

--- classic/kd_tree.cpp
+++ classic/kd_tree.cpp
@@ -15,6 +15,9 @@ struct point_t {
     std::array<double, 2> _coords;
 };
 
+/**
+ * Nodes are allocated in one call but referenced by pointers
+ */
 class kd_tree_t {
   public:
     kd_tree_t(const std::vector<point_t>& points)
@@ -120,6 +123,119 @@ class kd_tree_t {
     node_t* _root;
 };
 
+//int last_valid{}, last_ref{};
+/**
+ * Nodes are overallocated in one call and referenced by indexes
+ * This is slightly slower than kd_tree_t and uses more memory (36 < 4 * 12)
+ */
+class kd_tree_t2 {
+  public:
+    kd_tree_t2(const std::vector<point_t>& points)
+        : MAX_DIM(int(points[0]._coords.size()))
+        , _nodes(points.size() * 4) // 2 is too small
+    {
+        build(points, 1, 0);
+    }
+
+    std::vector<point_t> range_search(const point_t& lower, const point_t& upper) {
+        if (_nodes.empty())
+            return {};
+        std::vector<point_t> result;
+        range_search(1, lower, upper, result);
+        return result;
+    }
+
+    point_t nearest_neighbor(const point_t& target) {
+        if (_nodes.empty())
+            return {};
+        return nearest_neighbor(1, target, _nodes[0]._point, 0);
+    }
+
+  private:
+    struct node_t {
+        point_t _point{};
+        int _dim{-1};
+    };
+
+    void build(std::vector<point_t> points, const int node_id, int depth) {
+        if (points.empty())
+            return;
+//last_valid = std::max(last_valid, node_id);
+        const auto median = points.size() / 2;
+        const auto dim = depth % MAX_DIM;
+        std::nth_element(points.begin(), points.begin() + median, points.end(), [dim](const point_t& a, const point_t& b) {
+            return a._coords[dim] < b._coords[dim];
+        });
+
+        auto& node = _nodes[node_id];
+        node._point = points[median];
+        node._dim = dim;
+
+        std::vector<point_t> left_points(points.begin(), points.begin() + median);
+        std::vector<point_t> right_points(points.begin() + median + 1, points.end());
+        build(std::move(left_points), 2 * node_id, ++depth);
+        build(std::move(right_points), 2 * node_id + 1, depth);
+    }
+
+    void range_search(const int node_id, const point_t& lower, const point_t& upper, std::vector<point_t>& result) {
+//last_ref = std::max(last_ref, node_id);
+        const auto& node = _nodes[node_id];
+        if (node._dim == -1)
+            return;
+
+        bool inside = true;
+        auto low = lower._coords.begin();
+        auto hi  = upper._coords.begin();
+        for (const auto c : node._point._coords)
+            if (c < *low++ || *hi++ < c) {
+                inside = false;
+                break;
+            }
+
+        if (inside)
+            result.push_back(node._point);
+
+        const auto& left = _nodes[2 * node_id];
+        const auto& right = _nodes[2 * node_id + 1];
+        const auto dim_target = node._point._coords[node._dim];
+        if (0 <= left._dim && lower._coords[node._dim] <= dim_target)
+            range_search(2 * node_id, lower, upper, result);
+        if (0 <= right._dim && dim_target <= upper._coords[node._dim])
+            range_search(2 * node_id + 1, lower, upper, result);
+    }
+
+    point_t nearest_neighbor(const int node_id, const point_t& target, point_t best, int depth) {
+//last_ref = std::max(last_ref, node_id);
+        const auto& node = _nodes[node_id];
+        if (node._dim == -1)
+            return best;
+
+        auto best_dist = distance(best, target);
+        if (distance(node._point, target) < best_dist) {
+            best = node._point;
+            best_dist = distance(best, target);
+        }
+
+        const auto dim = depth % MAX_DIM;
+        const bool go_left = target._coords[dim] < node._point._coords[dim];
+        const auto near = go_left ? 2 * node_id : 2 * node_id + 1;
+
+        best = nearest_neighbor(near, target, best, ++depth);
+
+        if (std::abs(target._coords[dim] - node._point._coords[dim]) < best_dist)
+            best = nearest_neighbor(near ^ 1, target, best, depth);
+
+        return best;
+    }
+
+    double distance(const point_t& a, const point_t& b) {
+        return std::hypot(a._coords[0] - b._coords[0], a._coords[1] - b._coords[1]);
+    }
+
+    const int MAX_DIM;
+    std::vector<node_t> _nodes;
+};
+
 template <typename T = double>
 static T rand_in_range(const T ubound) {
     static std::random_device seed;
@@ -128,7 +244,7 @@ static T rand_in_range(const T ubound) {
     return dist(gen);
 };
 
-constinit const int NUM_OF_POINTS = 2'000'000;
+constinit const int NUM_OF_POINTS = (1 << 21) + 1;
 constinit const double MANHATTAN_RADIUS = 2.5;
 
 int main() {
@@ -138,7 +254,7 @@ int main() {
         return {double(rand_in_range(NUM_OF_POINTS)) / 2'000, double(rand_in_range(NUM_OF_POINTS)) / 2'000};
     });
 
-    kd_tree_t tree(points);
+    kd_tree_t2 tree(points);
     const point_t target = {871.0, 352.0};
     const point_t lower = {target._coords[0] - MANHATTAN_RADIUS, target._coords[1] - MANHATTAN_RADIUS};
     const point_t upper = {target._coords[0] + MANHATTAN_RADIUS, target._coords[1] + MANHATTAN_RADIUS};
@@ -153,7 +269,9 @@ int main() {
     const auto nearest = tree.nearest_neighbor(target);
     std::cout << std::format("Nearest neighbor: ({:5.1f}, {:5.1f})\n", nearest._coords[0], nearest._coords[1]);
 
-    return 0;
+//std::cerr << "Number of points: " << NUM_OF_POINTS << '\n';
+//std::cerr << "Last valid node index: " << last_valid << '\n';
+//std::cerr << "Last referenced node index: " << last_ref << '\n';
 }
 
 /*
